b658e903761e1891081af652cfa47e27600ec8d8
meshtag <prathameshtagore@gmail.com>
2021-10-04 18:06:42 +0530

    Apply clang formatting

--- Tests/TestImageGenerator.cpp
+++ Tests/TestImageGenerator.cpp
@@ -1,42 +1,43 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <string>
 
-void FillImage(cv::Mat img)
-{
+void FillImage(cv::Mat img) {
   for (std::ptrdiff_t row = 0; row < img.rows; ++row)
     for (std::ptrdiff_t col = 0; col < img.cols; ++col)
       img.at<uchar>(col, row) = row * img.rows + col;
 }
 
-enum class boundaryOption 
-{
+enum class boundaryOption {
   BORDER_CONSTANT,
   BORDER_REPLICATE,
   BORDER_REFLECT,
   BORDER_REFLECT101
 };
 
-void generateExpectedImage(cv::Mat img, cv::Mat expectedImg, int ddepth, cv::Mat kernel, 
-                           cv::Point anchor, double delta, boundaryOption option, std::string name)
-{
+void generateExpectedImage(cv::Mat img, cv::Mat expectedImg, int ddepth,
+                           cv::Mat kernel, cv::Point anchor, double delta,
+                           boundaryOption option, std::string name) {
   if (option == boundaryOption::BORDER_CONSTANT)
-    cv::filter2D(img, expectedImg, ddepth, kernel, anchor, delta, cv::BORDER_CONSTANT);
+    cv::filter2D(img, expectedImg, ddepth, kernel, anchor, delta,
+                 cv::BORDER_CONSTANT);
   else if (option == boundaryOption::BORDER_REPLICATE)
-    cv::filter2D(img, expectedImg, ddepth, kernel, anchor, delta, cv::BORDER_REPLICATE);
+    cv::filter2D(img, expectedImg, ddepth, kernel, anchor, delta,
+                 cv::BORDER_REPLICATE);
   else if (option == boundaryOption::BORDER_REFLECT)
-    cv::filter2D(img, expectedImg, ddepth, kernel, anchor, delta, cv::BORDER_REFLECT);
+    cv::filter2D(img, expectedImg, ddepth, kernel, anchor, delta,
+                 cv::BORDER_REFLECT);
   else if (option == boundaryOption::BORDER_REFLECT101)
-    cv::filter2D(img, expectedImg, ddepth, kernel, anchor, delta, cv::BORDER_REFLECT101);
+    cv::filter2D(img, expectedImg, ddepth, kernel, anchor, delta,
+                 cv::BORDER_REFLECT101);
 
   cv::imwrite(name, expectedImg);
 }
 
-int main(int argc, char **argv)
-{
-  if (argc != 4)
-  {
-    std::cout << "Invalid CLI arguments\nUsage : ImageHeight ImageWidth DestinationFolder\n";
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    std::cout << "Invalid CLI arguments\nUsage : ImageHeight ImageWidth "
+                 "DestinationFolder\n";
   }
 
   cv::Mat origImg, expImg;
@@ -50,26 +51,29 @@ int main(int argc, char **argv)
   cv::Point anchor;
   cv::Mat kernel = cv::Mat::ones(3, 3, CV_32F);
 
-  for (std::ptrdiff_t i = 0; i < kernel.rows; ++i)
-  {
-    for (std::ptrdiff_t j = 0; j < kernel.cols; ++j)
-    {
+  for (std::ptrdiff_t i = 0; i < kernel.rows; ++i) {
+    for (std::ptrdiff_t j = 0; j < kernel.cols; ++j) {
       // Put all expected images inside a folder and then test them from there.
       std::string partialName;
-      partialName = std::string(argv[3]) + "/ExpectedImage_" + std::to_string(i) + "_" + std::to_string(j) + "_";
+      partialName = std::string(argv[3]) + "/ExpectedImage_" +
+                    std::to_string(i) + "_" + std::to_string(j) + "_";
       anchor = cv::Point(i, j);
 
-      generateExpectedImage(origImg, expImg, ddepth, kernel, anchor, delta, 
-                            boundaryOption::BORDER_CONSTANT, partialName + "BORDER_CONSTANT.png");
+      generateExpectedImage(origImg, expImg, ddepth, kernel, anchor, delta,
+                            boundaryOption::BORDER_CONSTANT,
+                            partialName + "BORDER_CONSTANT.png");
 
-      generateExpectedImage(origImg, expImg, ddepth, kernel, anchor, delta, 
-                            boundaryOption::BORDER_REPLICATE, partialName + "BORDER_RELPICATE.png");
+      generateExpectedImage(origImg, expImg, ddepth, kernel, anchor, delta,
+                            boundaryOption::BORDER_REPLICATE,
+                            partialName + "BORDER_RELPICATE.png");
 
-      generateExpectedImage(origImg, expImg, ddepth, kernel, anchor, delta, 
-                            boundaryOption::BORDER_REFLECT, partialName + "BORDER_REFLECT.png");
+      generateExpectedImage(origImg, expImg, ddepth, kernel, anchor, delta,
+                            boundaryOption::BORDER_REFLECT,
+                            partialName + "BORDER_REFLECT.png");
 
-      generateExpectedImage(origImg, expImg, ddepth, kernel, anchor, delta, 
-                            boundaryOption::BORDER_REFLECT101, partialName + "BORDER_REFLECT101.png");
+      generateExpectedImage(origImg, expImg, ddepth, kernel, anchor, delta,
+                            boundaryOption::BORDER_REFLECT101,
+                            partialName + "BORDER_REFLECT101.png");
     }
   }
 }
