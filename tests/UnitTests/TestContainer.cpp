cfc5647d2cfd5b278fff5f0aeaeea521e3f2f962
zhanghb97 <hongbin2019@iscas.ac.cn>
2022-01-04 21:46:43 +0800

    [Utils][Container] Add copy and move constructors and assignment operators. (#11)

--- tests/UnitTests/TestContainer.cpp
+++ tests/UnitTests/TestContainer.cpp
@@ -195,6 +195,89 @@ TEST_F(MemRefTest, TransposeNCHWToNHWC) {
   ASSERT_ARRAY_EQ(transposed.getData(), true_data, n_data);
 }
 
+// Copy constructor.
+TEST_F(MemRefTest, CopyConstructor2DMemref) {
+  MemRef<float, 2> copyTemp = m2;
+  MemRef<float, 2> copy(copyTemp);
+  // Sizes.
+  intptr_t true_sizes[] = {2, 3};
+  size_t n_sizes = sizeof(true_sizes) / sizeof(intptr_t);
+  ASSERT_EQ(copy.getRank(), n_sizes);
+  ASSERT_ARRAY_EQ(copy.getSizes(), true_sizes, n_sizes);
+  // Strides.
+  intptr_t true_strides[] = {3, 1};
+  size_t n_strides = sizeof(true_strides) / sizeof(intptr_t);
+  ASSERT_EQ(copy.getRank(), n_strides);
+  ASSERT_ARRAY_EQ(copy.getStrides(), true_strides, n_strides);
+  // Data.
+  float true_data[] = {0., 1., 2., 3., 4., 5.};
+  size_t n_data = sizeof(true_data) / sizeof(float);
+  ASSERT_EQ(copy.getSize(), n_data);
+  ASSERT_ARRAY_EQ(copy.getData(), true_data, n_data);
+}
+
+// Copy assignment operator.
+TEST_F(MemRefTest, CopyAssignment2DMemref) {
+  MemRef<float, 2> copy = MemRef<float, 2>({1, 1});
+  copy = m2;
+  // Sizes.
+  intptr_t true_sizes[] = {2, 3};
+  size_t n_sizes = sizeof(true_sizes) / sizeof(intptr_t);
+  ASSERT_EQ(copy.getRank(), n_sizes);
+  ASSERT_ARRAY_EQ(copy.getSizes(), true_sizes, n_sizes);
+  // Strides.
+  intptr_t true_strides[] = {3, 1};
+  size_t n_strides = sizeof(true_strides) / sizeof(intptr_t);
+  ASSERT_EQ(copy.getRank(), n_strides);
+  ASSERT_ARRAY_EQ(copy.getStrides(), true_strides, n_strides);
+  // Data.
+  float true_data[] = {0., 1., 2., 3., 4., 5.};
+  size_t n_data = sizeof(true_data) / sizeof(float);
+  ASSERT_EQ(copy.getSize(), n_data);
+  ASSERT_ARRAY_EQ(copy.getData(), true_data, n_data);
+}
+
+// Move constructor.
+TEST_F(MemRefTest, MoveConstructor2DMemref) {
+  MemRef<float, 2> move = std::move(m2.transpose());
+  // Sizes.
+  intptr_t true_sizes[] = {3, 2};
+  size_t n_sizes = sizeof(true_sizes) / sizeof(intptr_t);
+  ASSERT_EQ(move.getRank(), n_sizes);
+  ASSERT_ARRAY_EQ(move.getSizes(), true_sizes, n_sizes);
+  // Strides.
+  intptr_t true_strides[] = {2, 1};
+  size_t n_strides = sizeof(true_strides) / sizeof(intptr_t);
+  ASSERT_EQ(move.getRank(), n_strides);
+  ASSERT_ARRAY_EQ(move.getStrides(), true_strides, n_strides);
+  // Data.
+  float true_data[] = {0., 3., 1., 4., 2., 5.};
+  size_t n_data = sizeof(true_data) / sizeof(float);
+  ASSERT_EQ(move.getSize(), n_data);
+  ASSERT_ARRAY_EQ(move.getData(), true_data, n_data);
+}
+
+// Move assignment operator.
+TEST_F(MemRefTest, MoveAssignment2DMemref) {
+  MemRef<float, 2> move = MemRef<float, 2>({1, 1});
+  move = std::move(m2.transpose());
+  // Sizes.
+  intptr_t true_sizes[] = {3, 2};
+  size_t n_sizes = sizeof(true_sizes) / sizeof(intptr_t);
+  ASSERT_EQ(move.getRank(), n_sizes);
+  ASSERT_ARRAY_EQ(move.getSizes(), true_sizes, n_sizes);
+  // Strides.
+  intptr_t true_strides[] = {2, 1};
+  size_t n_strides = sizeof(true_strides) / sizeof(intptr_t);
+  ASSERT_EQ(move.getRank(), n_strides);
+  ASSERT_ARRAY_EQ(move.getStrides(), true_strides, n_strides);
+  // Data.
+  float true_data[] = {0., 3., 1., 4., 2., 5.};
+  size_t n_data = sizeof(true_data) / sizeof(float);
+  ASSERT_EQ(move.getSize(), n_data);
+  ASSERT_ARRAY_EQ(move.getData(), true_data, n_data);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
