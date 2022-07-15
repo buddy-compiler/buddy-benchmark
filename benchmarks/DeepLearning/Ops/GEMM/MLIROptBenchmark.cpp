79eb08a401d9f07c63b5996ebdbab84b6d7b4f23
Yanwei Wang <weistking@hotmail.com>
2022-07-15 17:05:11 +0800

    [GEMM] Add support for Tilling Testing.

--- benchmarks/DeepLearning/Ops/GEMM/MLIROptBenchmark.cpp
+++ benchmarks/DeepLearning/Ops/GEMM/MLIROptBenchmark.cpp
@@ -25,20 +25,19 @@ namespace {
 
 // Declare the mobilenet C interface.
 extern "C" {
-void _mlir_ciface_gemm(MemRef<float, 2> *A, MemRef<float, 2> *B,
-                       MemRef<float, 2> *C);
+void _mlir_ciface_gemm(MemRef<double, 2> *A, MemRef<double, 2> *B,
+                       MemRef<double, 2> *C);
 }
 
 void BM_GEMM(benchmark::State &state) {
-  long M, N, K;
-  M = N = K = state.range(0);
+  long M = 2088, N = 2048, K = 2048;
   intptr_t sizesA[2] = {M, K};
   intptr_t sizesB[2] = {K, N};
   intptr_t sizesC[2] = {M, N};
 
-  MemRef<float, 2> A(sizesA, 1.0);
-  MemRef<float, 2> B(sizesB, 1.0);
-  MemRef<float, 2> C(sizesC, 0.0);
+  MemRef<double, 2> A(sizesA, 1.0);
+  MemRef<double, 2> B(sizesB, 1.0);
+  MemRef<double, 2> C(sizesC, 0.0);
 
   for (auto _ : state) {
     auto start = std::chrono::high_resolution_clock::now();
@@ -48,12 +47,14 @@ void BM_GEMM(benchmark::State &state) {
     auto elapsed_seconds =
         std::chrono::duration_cast<std::chrono::duration<long double>>(end -
                                                                        start);
-    state.counters["GFLOPS"] =
-        (2 * M * N * K) / (elapsed_seconds.count() * 1e9);
+    auto res = (2.0 * M * N * K * 1.0E9) / (elapsed_seconds.count());
+    std::cout << res << std::endl;
+    state.counters["GFLOPS"] = res;
   }
 }
 
 } // namespace
 
 // Register benchmarking function with different arguments.
-BENCHMARK(BM_GEMM)->DenseRange(512, 56320, 512);
+// BENCHMARK(BM_GEMM)->DenseRange(512, 56320, 512);
+BENCHMARK(BM_GEMM);
