e726fa2d40c00a7cf09c16e44a88714ea8783be2
amanchhaparia <amanchhaparia125@gmail.com>
2022-07-25 23:35:32 +0530

    correct style format

--- benchmarks/AudioProcessing/BuddyBiquadBenchmark.cpp
+++ benchmarks/AudioProcessing/BuddyBiquadBenchmark.cpp
@@ -1,4 +1,5 @@
-//===- BuddyBiquadBenchmark.cpp ---------------------------------------------------------===//
+//===- BuddyBiquadBenchmark.cpp
+//---------------------------------------------------------===//
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -31,39 +32,45 @@ extern "C" {
 void _mlir_ciface_buddy_biquad(MemRef<float, 1> *inputBuddyConv1D,
                                MemRef<float, 1> *kernelBuddyConv1D,
                                MemRef<float, 1> *outputBuddyConv1D);
-
 }
+
 namespace {
-univector<float,5>kernel = {0.226053, 0.452106, 0.226053, -0.404376, 0.308588}; 
-univector<float, 2000000> aud;
-univector<float> result;
-int sizeofKernel = kernel.size();
-int sizeofAud{aud.size()};
+univector<float, 5> kernel;
+biquad_params<float> bq = {biquad_lowpass(0.3, -1.0)};
+
+univector<float, 2000000> aud_buddy_biquad;
+univector<float, 2000000> result_buddy_biquad;
+intptr_t sizeofKernel{kernel.size()};
+intptr_t sizeofAud{aud_buddy_biquad.size()};
 
 // MemRef copys all data, so data here are actually not accessed.
-MemRef<float,1> kernelRef(reinterpret_cast<intptr_t *>(&sizeofKernel));
-MemRef<float,1> audRef(reinterpret_cast<intptr_t *>(&sizeofAud));
-MemRef<float,1> resRef(reinterpret_cast<intptr_t *>(&sizeofAud));
-}
+MemRef<float, 1> kernelRef(&sizeofKernel);
+MemRef<float, 1> audRef(&sizeofAud);
+MemRef<float, 1> resRef(&sizeofAud);
+} // namespace
 
 // Initialize univector.
 void initializeBuddyBiquad() {
-  audio_reader_wav<float> reader(open_file_for_reading(
+  audio_reader_wav<float> reader_buddy_biquad(open_file_for_reading(
       "../../benchmarks/AudioProcessing/Audios/NASA_Mars.wav"));
-  reader.read(aud.data(), aud.size());
-  kernelRef=std::move(MemRef<float,1>(kernel.data(),
-                              reinterpret_cast<intptr_t *>(&sizeofKernel)));
-  audRef=std::move(MemRef<float,1>(aud.data(),
-                          reinterpret_cast<intptr_t *>(&sizeofAud)));
-  resRef=std::move(MemRef<float,1>(reinterpret_cast<intptr_t *>(&sizeofAud)));
+  reader_buddy_biquad.read(aud_buddy_biquad.data(), aud_buddy_biquad.size());
+
+  kernel[0] = bq.b0;
+  kernel[1] = bq.b1;
+  kernel[2] = bq.b2;
+  kernel[3] = bq.a1;
+  kernel[4] = bq.a2;
+
+  kernelRef = std::move(MemRef<float, 1>(kernel.data(), &sizeofKernel));
+  audRef = std::move(MemRef<float, 1>(aud_buddy_biquad.data(), &sizeofAud));
+  resRef = std::move(MemRef<float, 1>(&sizeofAud));
 }
 
 // Benchmarking function.
 static void BUDDY_BIQUAD(benchmark::State &state) {
   for (auto _ : state) {
     for (int i = 0; i < state.range(0); ++i) {
-      // result = kfr::fir(aud, taps127);
-      _mlir_ciface_buddy_biquad(&audRef,&kernelRef,&resRef);
+      _mlir_ciface_buddy_biquad(&audRef, &kernelRef, &resRef);
     }
   }
 }
@@ -71,15 +78,17 @@ static void BUDDY_BIQUAD(benchmark::State &state) {
 // Register benchmarking function.
 BENCHMARK(BUDDY_BIQUAD)->Arg(1);
 
-// Generate result wav file.
+// Generate result_buddy_biquad wav file.
 void generateResultBuddyBiquad() {
-  MemRef<float,1> generateResult(reinterpret_cast<intptr_t *>(&sizeofAud));
-  _mlir_ciface_buddy_biquad(&audRef,&kernelRef,&generateResult);
+  println("-------------------------------------------------------");
+  println("[ Buddy Biquad Result Information ]");
+  MemRef<float, 1> generateResult(&sizeofAud);
+  _mlir_ciface_buddy_biquad(&audRef, &kernelRef, &generateResult);
 
-  audio_writer_wav<float> writer(open_file_for_writing("./ResultBuddyBiqaud.wav"),
-                                 audio_format{1 /* channel */,
-                                              audio_sample_type::i24,
-                                              100000 /* sample rate */});
+  audio_writer_wav<float> writer(
+      open_file_for_writing("./ResultBuddyBiqaud.wav"),
+      audio_format{1 /* channel */, audio_sample_type::i24,
+                   100000 /* sample rate */});
   writer.write(generateResult.getData(), generateResult.getSize());
   println("Sample Rate  = ", writer.format().samplerate);
   println("Channels     = ", writer.format().channels);
