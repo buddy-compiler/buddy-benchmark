7eee20cab3a0bc7be664c10ecd0ed34862d48a8e
BHbean <1216808064@qq.com>
2022-08-23 11:57:25 +0800

    reformat some files and add support for plotting single wave

--- plots/python/plot.py
+++ plots/python/plot.py
@@ -1,50 +1,74 @@
-"""
-===- plot.py -----------------------------------------------------------===
+# ===- plot.py ---------------------------------------------------------===
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-===----------------------------------------------------------------------===
+# ===--------------------------------------------------------------------===
 
-This is the starter of the figure-plotting modules.
+# This is the starter of the figure-plotting modules.
 
-===----------------------------------------------------------------------===
-"""
+# ===--------------------------------------------------------------------===
 
-#!/usr/bin/python3
+# !/usr/bin/python3
 import argparse
 from plotools.compare import compare_wave
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="This module is designed for figure plotting.")
-    parser.add_argument('file1', help='The path of the .wav file before process.')
-    parser.add_argument('file2', help='The path of the .wav file after process.')
-    parser.add_argument('-b', '--part', choices=['head', 'body', 'tail', 'all'], default='body',
-                        help='Choose a part of the time-domain figure to show. Possible values are "head", "body", '
-                             '"tail" and "all".')
+    parser = argparse.ArgumentParser(
+        prog='audio-plot',
+        description="This module is designed for figure plotting."
+        )
+    parser.add_argument(
+        'file1', help='The path of the .wav file before process.'
+        )
+    parser.add_argument(
+        'file2', nargs='?', default=None,
+        help='The path of the .wav file after process. If no file2 was \
+              specified, we will only plot the wave of file1.'
+        )
+    parser.add_argument('-b', '--part',
+                        choices=['head', 'body', 'tail', 'all'],
+                        default='body',
+                        help='Choose a part of the sound wave in the time-\
+                              domain to be plotted. Possible values are \
+                              "head", "body", "tail" and "all". If "head" is \
+                              specified, the first few points of the sound \
+                              wave will be plotted, and "last" stands for \
+                              the last few points, while "body" stands for the\
+                               center few points. When "all" is selected, the \
+                              whole sound wave will be plotted, and the "-p" \
+                              and "-n" options will be ignored.')
     parser.add_argument('-p', '--portion', type=float,
-                        help='Decide the portion(between 0 and 1) of the wave to be shown. Note once this argument '
-                             'was specified, the "--points" argument would be invalid.')
+                        help='Decide the portion(between 0 and 1) of the wave \
+                              to be shown. Note once this argument was \
+                              specified, the "--points" argument would be \
+                              invalid.')
     parser.add_argument('-n', '--points', type=int, default=5000,
-                        help='Decide the number of points of the wave to be shown.')
-    parser.add_argument('--nosave', action='store_false', help='Decide whether or not to save the figure.')
+                        help='Decide the number of points of the sound wave to\
+                               be plotted. The default number is 5000, which \
+                              is cosy enough to show the wave.')
+    parser.add_argument('--nosave', action='store_false',
+                        help='Decide whether to save the figure. If this \
+                              option is set, the result figure would only be \
+                              shown on the screen instead of being saved as a \
+                              file.')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
     print("Plotting now...")
-    compare_wave(args.file1, args.file2, part=args.part, 
+    compare_wave(args.file1, args.file2, part=args.part,
                  granularity=args.portion if args.portion else args.points,
                  save=args.nosave)
     print("Plotting done!")
--- plots/python/plotools/compare.py
+++ plots/python/plotools/compare.py
@@ -1,29 +1,27 @@
-"""
-===- compare.py -----------------------------------------------------------===
+# ===- compare.py ---------------------------------------------------------===
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-===----------------------------------------------------------------------===
+# ===--------------------------------------------------------------------===
 
-This file contains the main functions used to plot the figures.
+# This file contains the main functions used to plot the figures.
 
-===----------------------------------------------------------------------===
-"""
+# ===--------------------------------------------------------------------===
 
 import wave
 from typing import Tuple, Dict, Union
 import numpy as np
-from numpy.fft import fft, fftfreq
+from scipy.fft import rfft, rfftfreq
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import matplotlib.gridspec as gridspec
@@ -33,7 +31,8 @@ def get_info_and_samples(file: str) -> Tuple[Dict[str, int], np.ndarray]:
     """
     Parse the .wav file to get some basic information and the sample data.
     :param file: The path of the .wav file.
-    :return: A dictionary that contains basic information and a numpy array that contains the sample data.
+    :return: A dictionary that contains basic information and a numpy array
+             that contains the sample data.
     """
     # get basic information of the .wav file
     with wave.open(file, 'rb') as audio:
@@ -83,51 +82,62 @@ def get_frequency_domain(file: str):
     """
     Get the necessary arrays needed to plot the figure of frequency domain.
     :param file: The path of the .wav file.
-    :return: The power data after FFT on sample data array (y-axis) and the frequency array (x-axis).
+    :return: The power data after FFT on sample data array (y-axis) and the
+             frequency array (x-axis).
     """
     # get basic information and samples data
     info, samples = get_info_and_samples(file)
     framerate = info['framerate']
 
     # get power of the wave signal
-    fft_complex = fft(samples)
+    fft_complex = rfft(samples)
     power = np.abs(fft_complex)
 
     # get frequency array
-    freq = fftfreq(len(samples), 1 / framerate)
+    freq = rfftfreq(len(samples), 1 / framerate)
 
     return power, freq
 
 
-def compare_wave(file1: str, file2: str, part: str = "body", granularity: Union[int, float] = 5000, save: bool = True):
+def compare_wave(file1: str, file2: str = None, part: str = "body",
+                 granularity: Union[int, float] = 5000, save: bool = True):
     """
-    Plot the comparison figures between before and after the process in time-domain and frequency-domain.
+    Plot the comparison figures between before and after the process in
+    time-domain and frequency-domain.
     :param file1: The path of the .wav file before process.
-    :param file2: The path of the .wav file after process.
-    :param part: Choose a part of the time-domain figure to show. Possible values are "head", "body", "tail" and "all".
-    :param granularity: Choose how many points is to be plottes. It should be either a portion(float between 0~1) or a
-                        number(int).
-    :param save: A bool value to decide whether or not to save the figure.
+    :param file2: The path of the .wav file after process. If no file2 passed
+                  in, we will only plot waves of file1.
+    :param part: Choose a part of the time-domain figure to show. Possible
+                 values are "head", "body", "tail" and "all".
+    :param granularity: Choose how many points is to be plotted. It should be
+                        either a portion(float between 0~1) or a number(int).
+    :param save: A bool value to decide whether to save the figure.
     :return: None.
     """
     # plot time-domain figure
     before, time1 = get_time_domain(file1)
-    after, time2 = get_time_domain(file2)
-    # As we aim to compare signals between before and after the process, we have to promise that the time slot arrays
-    # remain the same.
-    assert (time1 == time2).all(), "Time slot arrays are not equal!"
+    if file2 is not None:
+        after, time2 = get_time_domain(file2)
+        # As we aim to compare signals between before and after the process, we
+        # have to promise that the time slot arrays remain the same.
+        assert (time1 == time2).all(), "Time slot arrays are not equal!"
 
-    # As the time-domain figure is usually too large to see the difference, we provide customized options to let the
-    # user decide which part should be plotted.
+    # As the time-domain figure is usually too large to see the difference, we
+    # provide customized options to let the user decide which part should be
+    # plotted.
     # calculate the number of points to be plotted
     N = len(time1)
-    if 0 <= granularity <= 1:                # the granularity could be portion
+    # the granularity could be portion
+    if 0 <= granularity <= 1:
         n = int(N * granularity)
         assert n > 0, "Few points to plot!"
-    elif granularity % 1 == 0:               # the granularity could be number of points to be plotted
+    # the granularity could be number of points to be plotted
+    elif granularity % 1 == 0:
         n = int(granularity)
     else:
-        raise ValueError("Parameter 'granularity' should be either a portion(float between 0~1) or a number(int)!")
+        raise ValueError(
+            "Parameter 'granularity' should be either a "
+            "portion(float between 0~1) or a number(int)!")
     # calculate the region to be plotted
     if part == "head":
         start = 0
@@ -143,32 +153,35 @@ def compare_wave(file1: str, file2: str, part: str = "body", granularity: Union[
         end = N
     before = before[start:end]
     time1 = time1[start:end]
-    after = after[start:end]
-    time2 = time2[start:end]
 
     fig = plt.figure(tight_layout=True)
     gs = gridspec.GridSpec(2, 2)
 
     ax = fig.add_subplot(gs[0, :])
     ax.plot(time1, before, linewidth=1, label="before")
-    ax.plot(time2, after, linewidth=1, label="after")
-    ax.legend()
+    if file2 is not None:
+        after = after[start:end]
+        time2 = time2[start:end]
+        ax.plot(time2, after, linewidth=1, label="after")
+        ax.legend()
     ax.set_title("Time Domain")
 
     # plot time-domain figure
-    # We reserve the whole plot to show the difference of the high-frequency part.
+    # We reserve the whole plot to show the difference of the high-frequency
+    # part.
     p_before, freq1 = get_frequency_domain(file1)
-    p_after, freq2 = get_frequency_domain(file2)
-    assert (freq1 == freq2).all(), "Frequency arrays are not equal!"
 
     ax = fig.add_subplot(gs[1, :])
     ax.semilogy(freq1, p_before, label="before")
-    ax.semilogy(freq2, p_after, color="orange", label="after")
-    ax.legend()
+    if file2 is not None:
+        p_after, freq2 = get_frequency_domain(file2)
+        assert (freq1 == freq2).all(), "Frequency arrays are not equal!"
+        ax.semilogy(freq2, p_after, color="orange", label="after")
+        ax.legend()
     ax.set_title("Frequency Domain")
 
     fig.align_labels()
 
     if save:
-        plt.savefig("compare_res.png", dpi=200)
+        plt.savefig("res.png", dpi=200)
     plt.show()
