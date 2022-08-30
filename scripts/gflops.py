326cbb3f7db6f8095070f952c2e56f7adeb93ed8
Yanwei Wang <weistking@hotmail.com>
2022-08-30 17:36:16 +0800

    update.

--- scripts/gflops.py
+++ scripts/gflops.py
@@ -1,4 +1,33 @@
+def calc_conv(p):
+    '''
+  long a = 1, b = factor, c = 16 * factor, d = 16 * factor,
+       e = 1, f = 32 * factor, g = 32 * factor;
+    '''
+
+    a = 1
+    b = p
+    c = 16 * p
+    d = 16 * p
+    e = 1
+    f = 32 * p
+    g = 32 * p
+
+    res = 2.0 * a * b * c * d * e * f * g 
+    # print(a, b, c, d, e, f, g, res)
+    return res
+
+def calc_gemm(p):
+    return 2.0 * p * p * p
+
+calc_dict = {
+        'BM_CONV': calc_conv,
+
+        'BM_GEMM': calc_gemm,
+        'BM_OPENCV_GEMM': calc_gemm
+    }
+    
 # skip first 3 line.
+
 for i in range(3):
     input()
 while True:
@@ -11,7 +40,7 @@ while True:
         it_times = int(it_times)
         base_size = int(base_size)
         
-        gflops = (2.0 * it_times * base_size * base_size * base_size) / time_cpu
+        gflops = (1.0 * it_times * calc_dict[name](base_size)) / time_cpu
         print(f'{name}\t\t {base_size}\t\t {gflops}')
     except EOFError:
         break
