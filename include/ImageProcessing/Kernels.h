0df69f7545b5f936e0c2bb2401cdbf2e671b8170
rishabh <rishabhsbali@gmail.com>
2022-10-31 00:32:32 +0530

    corrections

--- include/ImageProcessing/Kernels.h
+++ include/ImageProcessing/Kernels.h
@@ -89,13 +89,7 @@ static float random5x5KernelAlign[25] = {1, 9, 8, 8, 1,
                                          6, 6, 3, 3, 7, 
                                          1, 9, 5, 5, 7, 
                                          4, 5, 4, 3, 2, 
-                                         9, 8, 3, 8, 9};
-
-static uint8_t random5x5KernelAlignInt[25] = {1, 9, 8, 8, 1, 
-                                         6, 6, 3, 3, 7, 
-                                         1, 9, 5, 5, 7, 
-                                         4, 5, 4, 3, 2, 
-                                         9, 8, 3, 8, 9};                                         
+                                         9, 8, 3, 8, 9};                                       
 static int random5x5KernelRows = 5;
 static int random5x5KernelCols = 5;
 
@@ -105,15 +99,7 @@ static float random7x7KernelAlign[49] = {6, 3, 1, 5, 1, 9, 4,
                                         4, 5, 2, 6, 3, 9, 1, 
                                         7, 7, 2, 6, 7, 8, 6, 
                                         2, 8, 9, 2, 2, 7, 8, 
-                                        8, 9, 2, 3, 5, 3, 5};
-
-static uint8_t random7x7KernelAlignInt[49] = {6, 3, 1, 5, 1, 9, 4, 
-                                        0, 0, 8, 0, 5, 0, 2, 
-                                        9, 1, 7, 6, 1, 3, 8, 
-                                        4, 5, 2, 6, 3, 9, 1, 
-                                        7, 7, 2, 6, 7, 8, 6, 
-                                        2, 8, 9, 2, 2, 7, 8, 
-                                        8, 9, 2, 3, 5, 3, 5};                                        
+                                        8, 9, 2, 3, 5, 3, 5};                                      
 static int random7x7KernelRows = 7;
 static int random7x7KernelCols = 7;
 
@@ -125,17 +111,7 @@ static float random9x9KernelAlign[81] = {7, 9, 5, 0, 9, 8, 4, 9, 9,
                                          7, 2, 9, 8, 1, 4, 5, 7, 7, 
                                          7, 3, 4, 4, 3, 3, 4, 0, 7, 
                                          2, 7, 3, 5, 2, 3, 4, 2, 3, 
-                                         9, 5, 2, 7, 7, 4, 8, 7, 2};
-
-static uint8_t random9x9KernelAlignInt[81] = {7, 9, 5, 0, 9, 8, 4, 9, 9, 
-                                         4, 7, 0, 8, 3, 3, 2, 8, 7, 
-                                         1, 0, 5, 5, 9, 4, 1, 7, 0, 
-                                         6, 1, 5, 4, 1, 8, 4, 8, 4, 
-                                         4, 9, 5, 7, 5, 6, 7, 1, 3, 
-                                         7, 2, 9, 8, 1, 4, 5, 7, 7, 
-                                         7, 3, 4, 4, 3, 3, 4, 0, 7, 
-                                         2, 7, 3, 5, 2, 3, 4, 2, 3, 
-                                         9, 5, 2, 7, 7, 4, 8, 7, 2};                                         
+                                         9, 5, 2, 7, 7, 4, 8, 7, 2};                                       
 static int random9x9KernelRows = 9;
 static int random9x9KernelCols = 9;
 
@@ -149,19 +125,7 @@ static float random11x11KernelAlign[121] = {4, 5, 7, 6, 3, 6, 6, 6, 1, 9, 8,
                                             0, 6, 7, 9, 7, 5, 6, 6, 1, 8, 0,
                                             0, 7, 8, 3, 3, 8, 3, 2, 6, 9, 2,
                                             6, 5, 8, 1, 0, 7, 5, 8, 9, 7, 5,
-                                            6, 8, 4, 3, 7, 0, 6, 5, 1, 6, 5};
-
-static uint8_t random11x11KernelAlignInt[121] = {4, 5, 7, 6, 3, 6, 6, 6, 1, 9, 8,
-                                            5, 2, 0, 0, 9, 0, 1, 9, 7, 0, 8,
-                                            7, 8, 4, 4, 1, 5, 5, 3, 4, 9, 8,
-                                            4, 7, 1, 2, 3, 9, 3, 2, 9, 0, 7,
-                                            9, 2, 6, 0, 5, 7, 9, 6, 5, 8, 6,
-                                            0, 2, 7, 7, 7, 0, 3, 8, 0, 7, 5,
-                                            4, 9, 0, 5, 4, 5, 5, 7, 2, 6, 1,
-                                            0, 6, 7, 9, 7, 5, 6, 6, 1, 8, 0,
-                                            0, 7, 8, 3, 3, 8, 3, 2, 6, 9, 2,
-                                            6, 5, 8, 1, 0, 7, 5, 8, 9, 7, 5,
-                                            6, 8, 4, 3, 7, 0, 6, 5, 1, 6, 5};                                            
+                                            6, 8, 4, 3, 7, 0, 6, 5, 1, 6, 5};                                           
 static int random11x11KernelRows = 11;
 static int random11x11KernelCols = 11;
 
@@ -177,21 +141,7 @@ static float random13x13KernelAlign[169] = {8, 0, 6, 6, 7, 3, 4, 0, 6, 1, 5, 4, 
                                             3, 1, 5, 4, 0, 7, 9, 4, 0, 2, 9, 3, 2,
                                             0, 4, 4, 1, 4, 6, 2, 8, 9, 7, 6, 8, 2,
                                             8, 2, 0, 6, 6, 3, 0, 1, 8, 0, 8, 9, 6,
-                                            9, 1, 5, 2, 6, 8, 8, 2, 1, 4, 9, 3, 2};
-
-static uint8_t random13x13KernelAlignInt[169] = {8, 0, 6, 6, 7, 3, 4, 0, 6, 1, 5, 4, 3,
-                                            5, 8, 4, 7, 8, 7, 0, 7, 9, 1, 9, 4, 6,
-                                            5, 6, 0, 7, 6, 8, 9, 4, 7, 7, 7, 1, 9,
-                                            3, 4, 7, 8, 8, 4, 8, 4, 2, 6, 3, 4, 3,
-                                            5, 6, 5, 9, 4, 2, 5, 4, 9, 3, 4, 8, 7,
-                                            1, 7, 4, 4, 9, 0, 9, 6, 0, 9, 2, 8, 3,
-                                            4, 7, 8, 1, 2, 3, 7, 9, 4, 3, 1, 9, 7,
-                                            2, 4, 3, 3, 3, 5, 0, 0, 9, 1, 0, 0, 9,
-                                            0, 9, 2, 0, 4, 8, 9, 3, 9, 2, 8, 8, 1,
-                                            3, 1, 5, 4, 0, 7, 9, 4, 0, 2, 9, 3, 2,
-                                            0, 4, 4, 1, 4, 6, 2, 8, 9, 7, 6, 8, 2,
-                                            8, 2, 0, 6, 6, 3, 0, 1, 8, 0, 8, 9, 6,
-                                            9, 1, 5, 2, 6, 8, 8, 2, 1, 4, 9, 3, 2};                                            
+                                            9, 1, 5, 2, 6, 8, 8, 2, 1, 4, 9, 3, 2};                                          
 static int random13x13KernelRows = 13;
 static int random13x13KernelCols = 13;
 
@@ -212,12 +162,7 @@ static std::map<std::string, std::tuple<float*, int, int>> kernelMap = {
 };
 
 static std::map<std::string, std::tuple<uint8_t*, int, int>> kernelMap1 = {
-    {"random3x3KernelAlignInt", {random3x3KernelAlignInt, random3x3KernelRows, random3x3KernelCols}},
-    {"random5x5KernelAlignInt", {random5x5KernelAlignInt, random5x5KernelRows, random5x5KernelCols}},
-    {"random7x7KernelAlignInt", {random7x7KernelAlignInt, random7x7KernelRows, random7x7KernelCols}},
-    {"random9x9KernelAlignInt", {random9x9KernelAlignInt, random9x9KernelRows, random9x9KernelCols}},
-    {"random11x11KernelAlignInt", {random11x11KernelAlignInt, random11x11KernelRows, random11x11KernelCols}},
-    {"random13x13KernelAlignInt", {random13x13KernelAlignInt, random13x13KernelRows, random13x13KernelCols}}
+    {"random3x3KernelAlignInt", {random3x3KernelAlignInt, random3x3KernelRows, random3x3KernelCols}}
 };
 
 // clang-format on
