6c0a18be938cd1d6d0b0deb84d4f98e9c325348d
zwshan <shanzhw@mail2.sysu.edu.cn>
2023-08-22 03:22:58 +0000

    [OpOptimization]add TVM batchnorm cpu gpu version

---
+++ benchmarks/OpOptimization/BatchNormalization/TVM/CPU/batchNormalization_manual.py
@@ -0,0 +1,145 @@
+import tvm
+from tvm import te
+import numpy as np
+from tvm import topi
+import timeit
+import mxnet as mx
+
+
+# ------------------------------------------------------------------------------
+# Default Scheduling
+# ------------------------------------------------------------------------------
+# Save to the d2ltvm package.
+def get_bn_data_mxnet(c, n, ctx='cpu'):
+    ctx = getattr(mx, ctx)()
+    data, mean, var, gamma, beta, out = get_bn_data(c, n,
+                                      lambda x: mx.nd.array(x, ctx=ctx))
+    data, out = data.expand_dims(axis=0), out.expand_dims(axis=0)
+    return data, mean, var, gamma, beta, out
+
+
+def batch_norm_mxnet(data, mean, var, gamma, beta, out, eps=1e-5):
+    # use_global_stats=True to use the input mean and var instead of computing
+    # the mean and var of the input data.
+    # fix_gamma=False so that gamma won't be set to 1.
+    mx.nd.BatchNorm(data, gamma, beta, mean, var, eps,
+                    use_global_stats=True, fix_gamma=False, out=out)
+
+def batch_norm(c, n, eps=1e-5):
+    """batch normalization
+
+    c : channels
+    N : input width and height
+    eps : small positive value to prevent divide 0
+    """
+
+    X = te.placeholder((c, n, n), name='X')
+    Mean = te.placeholder((c, 1, 1), name='Mean')
+    Var = te.placeholder((c, 1, 1), name='Var')
+    Gamma = te.placeholder((c, 1, 1), name='Gamma')
+    Beta = te.placeholder((c, 1, 1), name='Beta')
+    C1 = topi.subtract(X,Mean)
+    C2 = topi.sqrt(Var + eps)
+    Y = C1 / C2 * Gamma + Beta
+    return X, Mean, Var, Gamma, Beta, Y
+   
+
+def get_bn_data(c, n, constructor=None):
+    """Return the batch norm data, mean, variance, gamma and beta tensors.
+       Also return the empty tensor for output.
+
+    c : channels
+    n : input width and height
+    constructor : user-defined tensor constructor
+    """
+    np.random.seed(0)
+    data = np.random.normal(size=(c, n, n)).astype('float32')
+    mean = np.random.normal(size=(c, 1, 1)).astype('float32')
+    var = np.random.normal(loc=1.0, size=(c, 1, 1)).astype('float32')
+    var = np.absolute(var)
+    gamma = np.random.normal(size=(c, 1, 1)).astype('float32')
+    beta = np.random.normal(size=(c, 1, 1)).astype('float32')
+    out = np.empty((c, n, n), dtype='float32')
+    if constructor:
+        data, mean, var, gamma, beta, out = \
+        (constructor(x) for x in [data, mean, var, gamma, beta, out])
+    return data, mean, var, gamma, beta, out
+
+
+def default_bn(size):
+    c, n = size[:]
+    X, Mean, Var, Gamma, Beta, Y = batch_norm(c, n)
+    sch = te.create_schedule(Y.op)
+    return sch, (X, Mean, Var, Gamma, Beta, Y)
+
+
+def optimized_bn(size):
+    sch, (X, Mean, Var, Gamma, Beta, Y) = default_bn(size)
+    te.schedule.AutoInlineInjective(sch)
+    c, h, w = Y.op.axis[0:3]
+    fused = sch[Y].fuse(c, h)
+    sch[Y].parallel(fused)
+    sch[Y].vectorize(w)
+    return sch, (X, Mean, Var, Gamma, Beta, Y)
+
+
+def bench_workload(workload):
+    """Benchmark a workload
+
+    workload: a method that accept a num_repeat argument
+    and return its total execution time
+    """
+    workload(1)  # warmup
+    time = workload(1)  # the time to run once
+    if time > 1: return time
+    # The number of repeats to measure at least 1 second
+    num_repeats = max(int(1.0 / time), 5)
+    return workload(num_repeats) / num_repeats
+
+
+def bn_timer_mxnet(c, n, ctx):
+    """Benchmark batch normalization in MXNet
+
+    c : channels
+    n : input width and height
+    ctx : compute ctx, e.g., cpu or gpu
+    """
+    timer = timeit.Timer(
+        setup='import mxnet as mx\n'
+        'from BatchNormalization_manual import get_bn_data_mxnet\n'
+        'from BatchNormalization_manual import batch_norm_mxnet\n'
+        'c, n = %d, %d\n'
+        'data, mean, var, gamma, beta, out = get_bn_data_mxnet(\n'
+        '    c, n, "%s")'%(c, n, ctx),
+        stmt='batch_norm_mxnet(data, mean, var, gamma, beta, out);'
+        'out.wait_to_read()')
+    return timer.timeit
+
+# Save to the d2ltvm package.
+def bench_bn_mxnet(size, ctx='cpu'):
+    """Return the execution times of MXNet batch norm"""
+    return bench_workload(bn_timer_mxnet(size[0], size[1], ctx))
+
+
+
+
+def main():
+  size = (32, 28)
+  mxnet_times = bench_bn_mxnet(size)
+  print(mxnet_times)
+#   sch, args = default_bn(size)
+#   mod = tvm.build(sch, args)
+
+
+#   data, mean, var, gamma, beta, out = get_bn_data(size[0], size[1], tvm.nd.array)
+#   mod(data, mean, var, gamma, beta, out)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+  
---
+++ benchmarks/OpOptimization/BatchNormalization/TVM/GPU/main.py
@@ -0,0 +1,153 @@
+# ===- main.py -----------------------------------------------------------------
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This is the entry of the TVM MatMul benchmark.
+#
+# ===---------------------------------------------------------------------------
+
+import numpy
+import timeit
+import tvm.testing
+import mxnet as mx
+
+from gpu_batch_norm_auto import *
+from gpu_batch_norm_manual import *
+
+
+
+# ------------------------------------------------------------------------------
+# User Configurable Variables
+# ------------------------------------------------------------------------------
+target = 'cuda'
+dev = tvm.cuda(0)
+size = (1024, 28)
+data, mean, var, gamma, beta, out = get_bn_data(size[0], size[1], tvm.nd.array)
+
+data = tvm.nd.array(data, dev)
+mean = tvm.nd.array(mean, dev)  
+var = tvm.nd.array(var, dev)
+gamma = tvm.nd.array(gamma, dev)
+beta = tvm.nd.array(beta, dev)
+out = tvm.nd.array(out, dev)
+
+test_input = data, mean, var, gamma, beta, out
+
+
+# ------------------------------------------------------------------------------
+# Helper Function
+# ------------------------------------------------------------------------------
+# def evaluate_operation(s, vars, target, inputs, standard, optimization, log):
+def evaluate_operation(s, vars, target, inputs, optimization, log):
+  """Evaluate operation correctness and print the performance information.
+  Args:
+    s: The schedule to be built.
+    vars: The argument lists to the function.
+    target: The target and option of the compilation.
+    inputs: The input tensors.
+    standard: The standard result for correctness evaluation.
+    optimization: The name of the optimization.
+    log: The log list.
+  """
+  target = 'cuda'
+  dev = tvm.cuda(0)
+  func = tvm.build(s, vars, target=target)
+  # dev = tvm.device(target.kind.name, 0)
+  data, mean, var, gamma, beta, out = inputs
+  # c = tvm.nd.array(numpy.zeros((M, N), dtype=dtype), dev)
+  func(data, mean, var, gamma, beta, out)
+
+  evaluator = func.time_evaluator(func.entry_name, dev, number=300)
+  mean_time = evaluator(data, mean, var, gamma, beta, out).mean * 1000  # Convert to milliseconds
+  log.append((optimization, mean_time))
+
+
+def report_performance(log):
+  """Convert the log into a performance table.
+  Args:
+    log: The log list.
+  """
+  baseline = log[-1][1]
+  header = "Benchmark".ljust(30) + "\t" + "Time".rjust(
+      10) + "\t" + "SpeedUp".rjust(10)
+  split_line = "-" * 70
+  print(split_line)
+  print(header)
+  print(split_line)
+  for result in log:
+    formatted_time = "{:.4f}".format(result[1])
+    formatted_performance = "{:.4f}".format(baseline / result[1])
+    print("\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m" %
+          (result[0].ljust(30), str(formatted_time + " ms").rjust(10),
+           str(formatted_performance).rjust(10)))
+
+
+
+
+def main():
+  # ----------------------------------------------------------------------------
+  # Initialization and Baseline
+  # ----------------------------------------------------------------------------
+  # Initialize the log list.
+  log = []
+
+  # dev = tvm.device(target.kind.name, 0)
+  # Generate random tensor for testing.
+
+  
+  
+  ctx = getattr(mx, "cpu")()
+  mxnet_times = bench_bn_mxnet(size)
+
+  # ----------------------------------------------------------------------------
+  # Register Benchmarks and Dump Report
+  # ----------------------------------------------------------------------------
+  # Register default schedule.
+  sch, arg_bufs = default_bn(size)
+  evaluate_operation(sch,
+                      arg_bufs,
+                      target=target,
+                      inputs=test_input,
+                      # standard=standard_res,
+                      optimization="default_bn",
+                      log=log)
+
+  sch, arg_bufs = optimized_bn(size)  
+  evaluate_operation(sch,
+                      arg_bufs,
+                      target=target,
+                      inputs=test_input,
+                      # standard=standard_res,
+                      optimization="optimized_bn",
+                      log=log)
+
+  sch, arg_bufs = gpu_pooling_autoschedule(size)
+  evaluate_operation(sch,
+                      arg_bufs,
+                      target=target,
+                      inputs=test_input,
+                      # standard=standard_res,
+                      optimization="auto_bn",
+                      log=log)
+
+  
+  # Register numpy case.
+  log.append(("mxnet_pooling", mxnet_times  * 1000))  # Milliseconds
+  # Dump the performance table.
+  report_performance(log)
+
+
+if __name__ == "__main__":
+  main()
