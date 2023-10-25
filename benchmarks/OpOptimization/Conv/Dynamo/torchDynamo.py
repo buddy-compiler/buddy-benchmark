05652a4d6f64a525cc6687ad107e808acd8761b2
zwshan <zw.shan@outlook.com>
2023-10-25 20:30:52 +0800

    using formatter to format my code

--- benchmarks/OpOptimization/Conv/Dynamo/torchDynamo.py
+++ benchmarks/OpOptimization/Conv/Dynamo/torchDynamo.py
@@ -11,8 +11,8 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the dynamo optimization for benchmark Conv on GPU.
-# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
-# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch
 # users are surfaced through the torch.compiler namespace.
 # which can automatically generate search spaces for optimizing tensor expressions.
 # See the pytorch license at: https://github.com/pytorch/pytorch/blob/main/LICENSE
@@ -22,12 +22,14 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 def conv_out_size(n, k, p, s):
     """Compute the output size by given input size n (width or height),
     kernel size k, padding p, and stride s
     Return output size (width or height)
     """
-    return (n - k + 2 * p)//s + 1
+    return (n - k + 2 * p) // s + 1
+
 
 def get_conv_data(oc, ic, n, k, p=0, s=1, constructor=None):
     """Return random 3-D data tensor, 3-D kernel tenor and empty 3-D output
@@ -40,25 +42,28 @@ def get_conv_data(oc, ic, n, k, p=0, s=1, constructor=None):
     constructor : user-defined tensor constructor
     """
     np.random.seed(0)
-    data = np.random.normal(size=(ic, n, n)).astype('float32')
-    weight = np.random.normal(size=(oc, ic, k, k)).astype('float32')
+    data = np.random.normal(size=(ic, n, n)).astype("float32")
+    weight = np.random.normal(size=(oc, ic, k, k)).astype("float32")
     on = conv_out_size(n, k, p, s)
-    out = np.empty((oc, on, on), dtype='float32')
+    out = np.empty((oc, on, on), dtype="float32")
     if constructor:
         data, weight, out = (constructor(x) for x in [data, weight, out])
     return data, weight, out
 
+
 def conv_torch(data, out, k, p, s):
     f = nn.Conv2d(data.shape[1], out.shape[1], kernel_size=k, stride=s, padding=p)
     return f
+
 
 def conv_compiled(data, out, k, p, s):
     f = nn.Conv2d(data.shape[1], out.shape[1], kernel_size=k, stride=s, padding=p)
     f_s = torch.compile(f)
     return f_s
 
+
 def get_conv_data_torch(c, n, k, p, s):
-    data, weight, out = get_conv_data(c, c, n, k, p, s,lambda x: torch.from_numpy(x))
-    data = data.unsqueeze(0)  
+    data, weight, out = get_conv_data(c, c, n, k, p, s, lambda x: torch.from_numpy(x))
+    data = data.unsqueeze(0)
     out = out.unsqueeze(0)
     return data, weight, out
