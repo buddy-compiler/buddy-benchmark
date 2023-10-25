ad26439d3c9aea6371dee38580687cef79ea42e8
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-25 11:09:07 +0800

    Update main.py

--- benchmarks/OpOptimization/BatchNormalization/CPU/main.py
+++ benchmarks/OpOptimization/BatchNormalization/CPU/main.py
@@ -1,7 +1,3 @@
-# ===- main.py -----------------------------------------------------------------
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,42 +10,34 @@
 #
 # ===---------------------------------------------------------------------------
 #
-# This is the entry of the TVM MatMul benchmark.
+# This file implements the entry for AutoScheduler BatchNormalization on CPU.
+# Autoscheduler is TVM's next-generation performance tuning tool, 
+# which can automatically generate search spaces for optimizing tensor expressions.
+# TVM is an Apache-2.0 licensed project.
+# See the TVM license at: https://github.com/apache/tvm/blob/main/LICENSE
 #
 # ===---------------------------------------------------------------------------
-
 import numpy
 import timeit
 import tvm.testing
 from batchNormalization_manual import *
 from batchNormalization_autoschedule import *
-  
-
-
 # ------------------------------------------------------------------------------
 # User Configurable Variables
 # ------------------------------------------------------------------------------
-
 # Define the size of the matrix.
-
 size = (1024, 28)
-
 # Choose the target for your target hardware plateform.
 target = tvm.target.Target(target="llvm", host="llvm")
 # target = tvm.target.Target("llvm -mcpu=core-avx2")
 # target = tvm.target.Target("llvm -mcpu=skylake-avx512")
 # target = tvm.target.arm_cpu("llvm -mattr=+neon")
-
-# Define the tensor data type.
 dtype = "float32"
-
-
 # ------------------------------------------------------------------------------
 # Helper Function
 # ------------------------------------------------------------------------------
 # def evaluate_operation(s, vars, target, inputs,standard , optimization, log):
 def evaluate_operation(s, vars, target, inputs, optimization, log):
-
   """Evaluate operation correctness and print the performance information.
   Args:
     s: The schedule to be built.
@@ -67,7 +55,6 @@ def evaluate_operation(s, vars, target, inputs, optimization, log):
   evaluator = func.time_evaluator(func.entry_name, dev, number=300)
   mean_time = evaluator(data, mean, var, gamma, beta, out).mean * 1000  # Convert to milliseconds
   log.append((optimization, mean_time))
-
 
 def report_performance(log):
   """Convert the log into a performance table.
@@ -88,32 +75,21 @@ def report_performance(log):
           (result[0].ljust(30), str(formatted_time + " ms").rjust(10),
            str(formatted_performance).rjust(10)))
 
-
-
-
-
 def main():
   # ----------------------------------------------------------------------------
   # Initialization and Baseline
   # ----------------------------------------------------------------------------
   # Initialize the log list.
   log = []
-
   dev = tvm.device(target.kind.name, 0)
-
   # Generate random tensor for testing.
   data, mean, var, gamma, beta, out = get_bn_data(size[0], size[1], tvm.nd.array)
   test_input = data, mean, var, gamma, beta, out
-
-
-  
   mxnet_times = bench_bn_mxnet(size)
-
   # ----------------------------------------------------------------------------
   # Register Benchmarks and Dump Report
   # ----------------------------------------------------------------------------
   # Register default schedule.
-
   s, arg_bufs = default_bn(size)
   evaluate_operation(s,
                       arg_bufs,
@@ -122,7 +98,6 @@ def main():
                       optimization="default_bn",
                       log=log)
 
-
   s, arg_bufs = optimized_bn(size)
   evaluate_operation(s,
                       arg_bufs,
@@ -130,7 +105,6 @@ def main():
                       inputs=test_input,
                       optimization="optimized_bn",
                       log=log)
-
 
   s, arg_bufs = batch_norm_auto_tuning_plus(size,target)
   evaluate_operation(s,
