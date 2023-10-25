1d4db4267eabad8a5ecda1f9bfd2f5fc19d2537e
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-25 17:42:05 +0800

    Update batchNormalization_iree.py

--- benchmarks/OpOptimization/BatchNormalization/batchNormalization_iree.py
+++ benchmarks/OpOptimization/BatchNormalization/batchNormalization_iree.py
@@ -1,3 +1,22 @@
+# You may obtain a copy of the License at
+#
+#     https://github.com/openxla/iree/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the IREE Optimization for benchmark BatchNormalization on GPU.
+# IREE (Intermediate Representation Execution Environment, pronounced as "eerie") 
+# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML) 
+# models to a unified IR that scales up to meet the needs of the datacenter and down 
+# to satisfy the constraints and special considerations of mobile and edge deployments.
+#
+# ===---------------------------------------------------------------------------
 import torch
 import torch.nn as nn
 import torch_mlir
