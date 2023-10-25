48440b6b78c3b792e310b036668e90421fd7bec0
zwshan <zw.shan@outlook.com>
2023-10-25 20:32:06 +0800

    using formatter to format my code

--- benchmarks/OpOptimization/Pooling/Dynamo/main.py
+++ benchmarks/OpOptimization/Pooling/Dynamo/main.py
@@ -11,8 +11,8 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the entry for benchmark Pooling on GPU.
-# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
-# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch
 # users are surfaced through the torch.compiler namespace.
 # which can automatically generate search spaces for optimizing tensor expressions.
 # See the pytorch license at: https://github.com/pytorch/pytorch/blob/main/LICENSE
@@ -21,85 +21,91 @@
 import numpy
 import time
 from torchDynamo import *
+
 # ------------------------------------------------------------------------------
 # User Configurable Variables
 # ------------------------------------------------------------------------------
 dtype = "float32"
+
+
 # ------------------------------------------------------------------------------
 # Helper Function
 # ------------------------------------------------------------------------------
 def evaluator(s, inputs, num):
-  all_time = []
-  for i in range(num):
-      torch.cuda.synchronize()
-      start = time.time()
-      result = s(inputs)
-      torch.cuda.synchronize()
-      end = time.time()
-      elapsed_time = end - start
-      all_time.append(elapsed_time)
-  average_time = sum(all_time) / num
-  return average_time
+    all_time = []
+    for i in range(num):
+        torch.cuda.synchronize()
+        start = time.time()
+        result = s(inputs)
+        torch.cuda.synchronize()
+        end = time.time()
+        elapsed_time = end - start
+        all_time.append(elapsed_time)
+    average_time = sum(all_time) / num
+    return average_time
+
 
 def evaluate_operation(s, inputs, optimization, log):
-  """Evaluate operation correctness and print the performance information.
-  Args:
-    s: The schedule to be built.
-    inputs: The input tensors.
-    optimization: The name of the optimization.
-    log: The log list.
-  """
-  mean_time = evaluator(s, inputs, 1)
-  log.append((optimization, mean_time))
+    """Evaluate operation correctness and print the performance information.
+    Args:
+      s: The schedule to be built.
+      inputs: The input tensors.
+      optimization: The name of the optimization.
+      log: The log list.
+    """
+    mean_time = evaluator(s, inputs, 1)
+    log.append((optimization, mean_time))
+
 
 def report_performance(log):
-  """Convert the log into a performance table.
-  Args:
-    log: The log list.
-  """
-  baseline = log[-1][1]
-  header = "Benchmark".ljust(20) + "\t" + "Time".rjust(
-      10) + "\t" + "SpeedUp".rjust(10)
-  split_line = "-" * 50
-  print(split_line)
-  print(header)
-  print(split_line)
-  for result in log:
-    formatted_time = "{:.2f}".format(result[1])
-    formatted_performance = "{:.2f}".format(baseline / result[1])
-    print("\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m" %
-          (result[0].ljust(20), str(formatted_time + " ms").rjust(10),
-           str(formatted_performance).rjust(10)))
+    """Convert the log into a performance table.
+    Args:
+      log: The log list.
+    """
+    baseline = log[-1][1]
+    header = (
+        "Benchmark".ljust(20) + "\t" + "Time".rjust(10) + "\t" + "SpeedUp".rjust(10)
+    )
+    split_line = "-" * 50
+    print(split_line)
+    print(header)
+    print(split_line)
+    for result in log:
+        formatted_time = "{:.2f}".format(result[1])
+        formatted_performance = "{:.2f}".format(baseline / result[1])
+        print(
+            "\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m"
+            % (
+                result[0].ljust(20),
+                str(formatted_time + " ms").rjust(10),
+                str(formatted_performance).rjust(10),
+            )
+        )
+
 
 def main():
-  # ----------------------------------------------------------------------------
-  # Initialization and Baseline
-  # ----------------------------------------------------------------------------
-  # Initialize the log list.
-  log = []
-  # Generate random tensor for testing.
-  size = (512, 64, 3)
-  c, n, k, p, s = size[0], size[0], size[1], size[2], 1
-  oc, ic, n, k, p, s = size[0], size[0], size[1], size[2], 1, 1
-  data, out_max = get_pool_data_torch(c, n, k, p, s)
-  # ----------------------------------------------------------------------------
-  # Register Benchmarks and Dump Report
-  # ----------------------------------------------------------------------------
-  # Register default schedule.
-  s_1 = pool_torch(k,p,s)
-  evaluate_operation(s_1,
-                     inputs=data,
-                     optimization="torch_pooling_default",
-                     log=log)
+    # ----------------------------------------------------------------------------
+    # Initialization and Baseline
+    # ----------------------------------------------------------------------------
+    # Initialize the log list.
+    log = []
+    # Generate random tensor for testing.
+    size = (512, 64, 3)
+    c, n, k, p, s = size[0], size[0], size[1], size[2], 1
+    oc, ic, n, k, p, s = size[0], size[0], size[1], size[2], 1, 1
+    data, out_max = get_pool_data_torch(c, n, k, p, s)
+    # ----------------------------------------------------------------------------
+    # Register Benchmarks and Dump Report
+    # ----------------------------------------------------------------------------
+    # Register default schedule.
+    s_1 = pool_torch(k, p, s)
+    evaluate_operation(s_1, inputs=data, optimization="torch_pooling_default", log=log)
 
-  s_2 = pool_compiled(k,p,s)
-  evaluate_operation(s_2,
-                     inputs=data,
-                     optimization="torch_pooling_dynamo",
-                     log=log)
+    s_2 = pool_compiled(k, p, s)
+    evaluate_operation(s_2, inputs=data, optimization="torch_pooling_dynamo", log=log)
 
-  report_performance(log)
+    report_performance(log)
+
 
 if __name__ == "__main__":
-  main()
-  
+    main()
--- benchmarks/OpOptimization/Pooling/Dynamo/torchDynamo.py
+++ benchmarks/OpOptimization/Pooling/Dynamo/torchDynamo.py
@@ -11,8 +11,8 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the dynamo optimization for benchmark Pooling on GPU.
-# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
-# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch
 # users are surfaced through the torch.compiler namespace.
 # which can automatically generate search spaces for optimizing tensor expressions.
 # See the pytorch license at: https://github.com/pytorch/pytorch/blob/main/LICENSE
@@ -22,12 +22,14 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 def conv_out_size(n, k, p, s):
     """Compute the output size by given input size n (width or height),
     kernel size k, padding p, and stride s
     Return output size (width or height)
     """
-    return (n - k + 2 * p)//s + 1
+    return (n - k + 2 * p) // s + 1
+
 
 def get_conv_data(oc, ic, n, k, p=0, s=1, constructor=None):
     """Return random 3-D data tensor, 3-D kernel tenor and empty 3-D output
@@ -40,25 +42,28 @@ def get_conv_data(oc, ic, n, k, p=0, s=1, constructor=None):
     constructor : user-defined tensor constructor
     """
     np.random.seed(0)
-    data = np.random.normal(size=(ic, n, n)).astype('float32')
-    weight = np.random.normal(size=(oc, ic, k, k)).astype('float32')
+    data = np.random.normal(size=(ic, n, n)).astype("float32")
+    weight = np.random.normal(size=(oc, ic, k, k)).astype("float32")
     on = conv_out_size(n, k, p, s)
-    out = np.empty((oc, on, on), dtype='float32')
+    out = np.empty((oc, on, on), dtype="float32")
     if constructor:
         data, weight, out = (constructor(x) for x in [data, weight, out])
     return data, weight, out
 
+
 def get_pool_data_torch(c, n, k, p, s):
-    data, _, out = get_conv_data(c, c, n, k, p, s,lambda x: torch.from_numpy(x))
-    data = data.unsqueeze(0)  
+    data, _, out = get_conv_data(c, c, n, k, p, s, lambda x: torch.from_numpy(x))
+    data = data.unsqueeze(0)
     out = out.unsqueeze(0)
     return data, out
+
 
 def pool_torch(k, p, s):
     f = nn.MaxPool2d(k, s, p)
     return f
 
-def pool_compiled(k,p,s):
+
+def pool_compiled(k, p, s):
     f = nn.MaxPool2d(k, s, p)
     f_compiled = torch.compile(f)
     return f_compiled
