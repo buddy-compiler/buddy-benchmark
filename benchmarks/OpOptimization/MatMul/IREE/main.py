9e0ac92d9a017490cab1a448fefd77478c2f0cbe
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-25 16:51:55 +0800

    Update main.py

--- benchmarks/OpOptimization/MatMul/IREE/main.py
+++ benchmarks/OpOptimization/MatMul/IREE/main.py
@@ -1,3 +1,23 @@
+# You may obtain a copy of the License at
+#
+#     https://github.com/openxla/iree/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the IREE entry for benchmark Matmul on GPU.
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# users are surfaced through the torch.compiler namespace.
+# which can automatically generate search spaces for optimizing tensor expressions.
+# See the pytorch license at: https://github.com/openxla/iree/blob/main/LICENSE
+#
+# ===---------------------------------------------------------------------------
 import numpy as np
 import time
 from matmul_iree import *
