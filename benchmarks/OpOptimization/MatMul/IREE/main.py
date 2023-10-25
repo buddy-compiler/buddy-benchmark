c86b5817f2a257bb6279160a5f64053d52b54276
zwshan <zw.shan@outlook.com>
2023-10-25 21:09:25 +0800

    using formatter to format code

--- benchmarks/OpOptimization/MatMul/IREE/main.py
+++ benchmarks/OpOptimization/MatMul/IREE/main.py
@@ -11,123 +11,132 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the IREE entry for benchmark matmul on GPU.
-# IREE (Intermediate Representation Execution Environment, pronounced as "eerie") 
-# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML) 
-# models to a unified IR that scales up to meet the needs of the datacenter and down 
+# IREE (Intermediate Representation Execution Environment, pronounced as "eerie")
+# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML)
+# models to a unified IR that scales up to meet the needs of the datacenter and down
 # to satisfy the constraints and special considerations of mobile and edge deployments.
 #
 # ===---------------------------------------------------------------------------
 import numpy as np
 import time
 from matmul_iree import *
+
 # ------------------------------------------------------------------------------
 # User Configurable Variables
 # ------------------------------------------------------------------------------
 dtype = "float32"
+
+
 # ------------------------------------------------------------------------------
 # Helper Function
 # ------------------------------------------------------------------------------
 def iree_evaluator(s, inputs, num):
-  result = s.forward(inputs)
-  all_time = []
-  for i in range(num):
-      start = time.time()
-      s.forward(inputs)
-      end = time.time()
-      elapsed_time = end - start
-      all_time.append(elapsed_time)
-  average_time = sum(all_time) / num
-  return average_time
+    result = s.forward(inputs)
+    all_time = []
+    for i in range(num):
+        start = time.time()
+        s.forward(inputs)
+        end = time.time()
+        elapsed_time = end - start
+        all_time.append(elapsed_time)
+    average_time = sum(all_time) / num
+    return average_time
 
-def numpy_evaluator(a_tensor,b_tensor,num):
-  a_tensor_np = a_tensor.numpy()
-  b_tensor_np = b_tensor.numpy()
-  all_time = []
-  for i in range(num):
-      start = time.time()
-      np.dot(a_tensor_np,b_tensor_np)
-      end = time.time()
-      elapsed_time = end - start
-      all_time.append(elapsed_time)
-  average_time = sum(all_time) / num
-  return average_time
+
+def numpy_evaluator(a_tensor, b_tensor, num):
+    a_tensor_np = a_tensor.numpy()
+    b_tensor_np = b_tensor.numpy()
+    all_time = []
+    for i in range(num):
+        start = time.time()
+        np.dot(a_tensor_np, b_tensor_np)
+        end = time.time()
+        elapsed_time = end - start
+        all_time.append(elapsed_time)
+    average_time = sum(all_time) / num
+    return average_time
+
 
 def evaluator(s, inputs, num):
-  result = s(inputs)
-  all_time = []
-  for i in range(num):
-      start = time.time()
-      s(inputs)
-      end = time.time()
-      elapsed_time = end - start
-      all_time.append(elapsed_time)
-  average_time = sum(all_time) / num
-  return average_time
+    result = s(inputs)
+    all_time = []
+    for i in range(num):
+        start = time.time()
+        s(inputs)
+        end = time.time()
+        elapsed_time = end - start
+        all_time.append(elapsed_time)
+    average_time = sum(all_time) / num
+    return average_time
+
 
 def evaluate_operation(s, inputs, optimization, log):
-  """Evaluate operation correctness and print the performance information.
-  Args:
-    s: The schedule to be built.
-    inputs: The input tensors.
-    optimization: The name of the optimization.
-    log: The log list.
-  """
-  if optimization == "IREE":
-    mean_time = iree_evaluator(s, inputs, 10)
-  else:
-    mean_time = evaluator(s, inputs, 10)
-  log.append((optimization, mean_time))
+    """Evaluate operation correctness and print the performance information.
+    Args:
+      s: The schedule to be built.
+      inputs: The input tensors.
+      optimization: The name of the optimization.
+      log: The log list.
+    """
+    if optimization == "IREE":
+        mean_time = iree_evaluator(s, inputs, 10)
+    else:
+        mean_time = evaluator(s, inputs, 10)
+    log.append((optimization, mean_time))
+
 
 def report_performance(log):
-  """Convert the log into a performance table.
-  Args:
-    log: The log list.
-  """
-  baseline = log[-1][1]
-  header = "Benchmark".ljust(20) + "\t" + "Time".rjust(
-      10) + "\t" + "SpeedUp".rjust(10)
-  split_line = "-" * 50
-  print(split_line)
-  print(header)
-  print(split_line)
-  for result in log:
-    formatted_time = "{:.2f}".format(result[1])
-    formatted_performance = "{:.2f}".format(baseline / result[1])
-    print("\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m" %
-          (result[0].ljust(20), str(formatted_time + " ms").rjust(10),
-           str(formatted_performance).rjust(10)))
+    """Convert the log into a performance table.
+    Args:
+      log: The log list.
+    """
+    baseline = log[-1][1]
+    header = (
+        "Benchmark".ljust(20) + "\t" + "Time".rjust(10) + "\t" + "SpeedUp".rjust(10)
+    )
+    split_line = "-" * 50
+    print(split_line)
+    print(header)
+    print(split_line)
+    for result in log:
+        formatted_time = "{:.2f}".format(result[1])
+        formatted_performance = "{:.2f}".format(baseline / result[1])
+        print(
+            "\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m"
+            % (
+                result[0].ljust(20),
+                str(formatted_time + " ms").rjust(10),
+                str(formatted_performance).rjust(10),
+            )
+        )
+
 
 def main():
-  # ----------------------------------------------------------------------------
-  # Initialization and Baseline
-  # ----------------------------------------------------------------------------
-  # Initialize the log list.
-  log = []
-  # Generate random tensor for testing.
-  a_dim1 = 2048
-  a_dim2 = 1024
-  b_dim1 = a_dim2
-  b_dim2 = 2048
-  model, b_tensor= torch_matrix_multiply(b_dim1, b_dim2)
-  a_tensor = torch.randn(a_dim1, a_dim2)
-  example_input = a_tensor
-  # ----------------------------------------------------------------------------
-  # Register Benchmarks and Dump Report
-  # ----------------------------------------------------------------------------
-  # Register default schedule.
-  invoker = iree_matrix_multiply(model, example_input)
-  evaluate_operation(invoker,
-                     inputs=example_input,
-                     optimization="IREE",
-                     log=log)
-  s = model
-  evaluate_operation(s,
-                     inputs=example_input,
-                     optimization="torch_cpu",
-                     log=log)
-  np_average_time = numpy_evaluator(a_tensor,b_tensor,10)
-  log.append(("numpy", np_average_time))
-  report_performance(log)
+    # ----------------------------------------------------------------------------
+    # Initialization and Baseline
+    # ----------------------------------------------------------------------------
+    # Initialize the log list.
+    log = []
+    # Generate random tensor for testing.
+    a_dim1 = 2048
+    a_dim2 = 1024
+    b_dim1 = a_dim2
+    b_dim2 = 2048
+    model, b_tensor = torch_matrix_multiply(b_dim1, b_dim2)
+    a_tensor = torch.randn(a_dim1, a_dim2)
+    example_input = a_tensor
+    # ----------------------------------------------------------------------------
+    # Register Benchmarks and Dump Report
+    # ----------------------------------------------------------------------------
+    # Register default schedule.
+    invoker = iree_matrix_multiply(model, example_input)
+    evaluate_operation(invoker, inputs=example_input, optimization="IREE", log=log)
+    s = model
+    evaluate_operation(s, inputs=example_input, optimization="torch_cpu", log=log)
+    np_average_time = numpy_evaluator(a_tensor, b_tensor, 10)
+    log.append(("numpy", np_average_time))
+    report_performance(log)
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- benchmarks/OpOptimization/MatMul/IREE/matmul_iree.py
+++ benchmarks/OpOptimization/MatMul/IREE/matmul_iree.py
@@ -11,9 +11,9 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the IREE optimization for benchmark matmul on GPU.
-# IREE (Intermediate Representation Execution Environment, pronounced as "eerie") 
-# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML) 
-# models to a unified IR that scales up to meet the needs of the datacenter and down 
+# IREE (Intermediate Representation Execution Environment, pronounced as "eerie")
+# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML)
+# models to a unified IR that scales up to meet the needs of the datacenter and down
 # to satisfy the constraints and special considerations of mobile and edge deployments.
 #
 # ===---------------------------------------------------------------------------
@@ -24,21 +24,27 @@ import iree_torch
 import io
 import numpy as np
 
+
 class MatrixMultiplication(nn.Module):
     def __init__(self, weight):
         super(MatrixMultiplication, self).__init__()
-        self.weight = nn.Parameter(weight)  
+        self.weight = nn.Parameter(weight)
+
     def forward(self, x):
         result = torch.mm(x, self.weight)
         return result
 
+
 def torch_matrix_multiply(b_dim1, b_dim2):
     weight = torch.randn(b_dim1, b_dim2)
     model = MatrixMultiplication(weight)
-    return model,weight
+    return model, weight
+
 
 def iree_matrix_multiply(model, example_input):
-    linalg_on_tensors_mlir = torch_mlir.compile(model,example_input,output_type="linalg-on-tensors",use_tracing=False)
+    linalg_on_tensors_mlir = torch_mlir.compile(
+        model, example_input, output_type="linalg-on-tensors", use_tracing=False
+    )
     iree_backend = "llvm-cpu"
     iree_vmfb = iree_torch.compile_to_vmfb(linalg_on_tensors_mlir, iree_backend)
     invoker = iree_torch.load_vmfb(iree_vmfb, iree_backend)
