e7d4219dea31c46cbcbd9d8562930b97a84ce96d
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-25 15:42:59 +0800

    Update main.py

--- benchmarks/OpOptimization/BatchNormalization/Dynamo/main.py
+++ benchmarks/OpOptimization/BatchNormalization/Dynamo/main.py
@@ -1,3 +1,23 @@
+# You may obtain a copy of the License at
+#
+#     https://github.com/pytorch/pytorch/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the dynamo entry for benchmark BatchNormalization on GPU.
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# users are surfaced through the torch.compiler namespace.
+# which can automatically generate search spaces for optimizing tensor expressions.
+# See the pytorch license at: https://github.com/pytorch/pytorch/blob/main/LICENSE
+#
+# ===---------------------------------------------------------------------------
 import numpy
 import time
 from torchDynamo import *
@@ -5,11 +25,9 @@ from torchDynamo import *
 # User Configurable Variables
 # ------------------------------------------------------------------------------
 dtype = "float32"
-
 # ------------------------------------------------------------------------------
 # Helper Function
 # ------------------------------------------------------------------------------
-
 def evaluator(s, inputs, num):
   all_time = []
   data, mean, var, gamma, beta, out = inputs
@@ -21,12 +39,8 @@ def evaluator(s, inputs, num):
       end = time.time()
       elapsed_time = end - start
       all_time.append(elapsed_time)
-
-
   average_time = sum(all_time) / num
-
   return average_time
-
 
 def evaluate_operation(s, inputs, optimization, log):
   """Evaluate operation correctness and print the performance information.
@@ -41,7 +55,6 @@ def evaluate_operation(s, inputs, optimization, log):
   """
   mean_time = evaluator(s, inputs, 1)
   log.append((optimization, mean_time))
-
 
 def report_performance(log):
   """Convert the log into a performance table.
@@ -62,30 +75,24 @@ def report_performance(log):
           (result[0].ljust(20), str(formatted_time + " ms").rjust(10),
            str(formatted_performance).rjust(10)))
 
-
 def main():
   # ----------------------------------------------------------------------------
   # Initialization and Baseline
   # ----------------------------------------------------------------------------
   # Initialize the log list.
   log = []
-
   # Generate random tensor for testing.
   size = (4096, 1024)
   data, mean, var, gamma, beta, out = get_bn_data(size[0], size[1])
-
-
   # ----------------------------------------------------------------------------
   # Register Benchmarks and Dump Report
   # ----------------------------------------------------------------------------
   # Register default schedule.
-
   s_1 = batch_norm_default()
   evaluate_operation(s_1,
                      inputs=(data, mean, var, gamma, beta, out),
                      optimization="torch_broAdd_default",
                      log=log)
-
 
   s_2 = batch_norm_compiled()
   evaluate_operation(s_2,
@@ -93,10 +100,7 @@ def main():
                      optimization="torch_broAdd_compiled",
                      log=log)
 
- 
-
   report_performance(log)
-
 
 if __name__ == "__main__":
   main()
