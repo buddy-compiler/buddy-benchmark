63e60d3a2e4aa048b77dd7530d43a21723e19f4b
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-25 14:44:37 +0800

    Update convolution_auto.py

--- benchmarks/OpOptimization/Convolution/TVM/CPU/convolution_auto.py
+++ benchmarks/OpOptimization/Convolution/TVM/CPU/convolution_auto.py
@@ -1,14 +1,31 @@
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the auto optimization for benchmark Convolution on CPU.
+# Autoscheduler is TVM's next-generation performance tuning tool, 
+# which can automatically generate search spaces for optimizing tensor expressions.
+# TVM is an Apache-2.0 licensed project.
+# See the TVM license at: https://github.com/apache/tvm/blob/main/LICENSE
+#
+# ===---------------------------------------------------------------------------
 import tvm
 from tvm import autotvm
 from tvm import te, auto_scheduler
 from convolution_manual import *
 import numpy as np
 
-
 @auto_scheduler.register_workload
 def conv_default(oc, ic, nh, nw, kh, kw, ph=0, pw=0, sh=1, sw=1):
     """Convolution
-
     oc, ic : output and input channels
     nh, nw : input width and height
     kh, kw : kernel width and height
@@ -33,15 +50,10 @@ def conv_default(oc, ic, nh, nw, kh, kw, ph=0, pw=0, sh=1, sw=1):
             axis=[ric, rkh, rkw]), name='Y')
     return X, K, Y, PaddedX
 
-
-
 def conv_auto(oc, ic, n, k, p, s):
     target = tvm.target.Target(target="llvm", host="llvm")
-
     task = tvm.auto_scheduler.SearchTask(func=conv_default, args= (oc, ic, n, n, k, k, p, p, s, s), target=target)
-
     print("==========conv_autoschedule=========")
-
     log_file = "conv_autoschedule.log"
     measure_ctx = None
     tune_option = auto_scheduler.TuningOptions(
