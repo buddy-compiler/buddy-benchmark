6898943df1377d7bcb7ed5738d1c5d53b21d28c3
zwshan <zw.shan@outlook.com>
2023-10-25 21:16:24 +0800

    using formatter to format code

--- benchmarks/OpOptimization/BatchMatmul/TVM/CPU/batch_matmul_cpu.py
+++ benchmarks/OpOptimization/BatchMatmul/TVM/CPU/batch_matmul_cpu.py
@@ -11,7 +11,7 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the AutoScheduler for BatchMatMul on CPU.
-# Autoscheduler is TVM's next-generation performance tuning tool, 
+# Autoscheduler is TVM's next-generation performance tuning tool,
 # which can automatically generate search spaces for optimizing tensor expressions.
 # TVM is an Apache-2.0 licensed project.
 # See the TVM license at: https://github.com/apache/tvm/blob/main/LICENSE
@@ -23,41 +23,50 @@ from tvm import topi
 import numpy as np
 from tvm import te, auto_scheduler
 
+
 @auto_scheduler.register_workload
-def batchMatmul_default(batch,M,K,N):
-  A = tvm.te.placeholder((batch, M, K), name='A')
-  B = tvm.te.placeholder((batch, K, N), name='B')
-  k = tvm.te.reduce_axis((0, K), 'k')
-  C = tvm.te.compute((batch, M, N),
-          lambda b, y, x: tvm.te.sum(A[b, y, k] * B[b, k, x], axis = k),
-          name = 'C')
-  s = tvm.te.create_schedule(C.op)
-  return [A,B,C]
+def batchMatmul_default(batch, M, K, N):
+    A = tvm.te.placeholder((batch, M, K), name="A")
+    B = tvm.te.placeholder((batch, K, N), name="B")
+    k = tvm.te.reduce_axis((0, K), "k")
+    C = tvm.te.compute(
+        (batch, M, N),
+        lambda b, y, x: tvm.te.sum(A[b, y, k] * B[b, k, x], axis=k),
+        name="C",
+    )
+    s = tvm.te.create_schedule(C.op)
+    return [A, B, C]
+
 
 def batchMatmul_auto_tuning(shape, target):
-  target = tvm.target.Target(target)
-  batch, M, K, N = shape
-  task = tvm.auto_scheduler.SearchTask(func=batchMatmul_default, args=(batch, M, K, N), target=target)
-  print("==========batchMatmul_auto_tuning=========")
-  log_file = "batchMatmul_auto_tuning.log"
-  measure_ctx = None
-  tune_option = auto_scheduler.TuningOptions(
-    num_measure_trials=60,
-    measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
-    verbose=1,
+    target = tvm.target.Target(target)
+    batch, M, K, N = shape
+    task = tvm.auto_scheduler.SearchTask(
+        func=batchMatmul_default, args=(batch, M, K, N), target=target
+    )
+    print("==========batchMatmul_auto_tuning=========")
+    log_file = "batchMatmul_auto_tuning.log"
+    measure_ctx = None
+    tune_option = auto_scheduler.TuningOptions(
+        num_measure_trials=60,
+        measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
+        verbose=1,
     )
     # vervose to determine whether output or not
-  task.tune(tune_option)
-  sch, args = task.apply_best(log_file)
-  return sch,args
+    task.tune(tune_option)
+    sch, args = task.apply_best(log_file)
+    return sch, args
 
-def batchMatmul_manual(batch,M,K,N):
-    A = tvm.te.placeholder((batch, M, K), name='A')
-    B = tvm.te.placeholder((batch, K, N), name='B')
-    k = tvm.te.reduce_axis((0, K), 'k')
-    C = tvm.te.compute((batch, M, N),
-    lambda b, y, x: tvm.te.sum(A[b, y, k] * B[b, k, x], axis = k),
-    name = 'C')
+
+def batchMatmul_manual(batch, M, K, N):
+    A = tvm.te.placeholder((batch, M, K), name="A")
+    B = tvm.te.placeholder((batch, K, N), name="B")
+    k = tvm.te.reduce_axis((0, K), "k")
+    C = tvm.te.compute(
+        (batch, M, N),
+        lambda b, y, x: tvm.te.sum(A[b, y, k] * B[b, k, x], axis=k),
+        name="C",
+    )
     # schedule optimization
     s = tvm.te.create_schedule(C.op)
     bn = 32
@@ -70,28 +79,31 @@ def batchMatmul_manual(batch,M,K,N):
     arg_bufs = [A, B, C]
     return s, arg_bufs
 
-def batchMatmul_numpy(shape,a_np,b_np):
-  batch_size,M,K,N = shape
-  c_np = np.zeros((batch_size, M, N), dtype=np.float32)
-  for bs in range(batch_size):
-      c_np[bs, :, :] = np.dot(a_np[bs, :, :], b_np[bs, :, :])
+
+def batchMatmul_numpy(shape, a_np, b_np):
+    batch_size, M, K, N = shape
+    c_np = np.zeros((batch_size, M, N), dtype=np.float32)
+    for bs in range(batch_size):
+        c_np[bs, :, :] = np.dot(a_np[bs, :, :], b_np[bs, :, :])
+
 
 def main():
-  target = tvm.target.Target(target="llvm", host="llvm")
-  batch_size = 64
-  M = 64
-  K = 128
-  N = 256
-  a_np = np.random.rand(batch_size, M, K).astype(np.float32)
-  b_np = np.random.rand(batch_size, K, N).astype(np.float32)
-  c_np = np.zeros((batch_size, M, N), dtype=np.float32)
-  a = tvm.nd.array(a_np)
-  b = tvm.nd.array(b_np)
-  c = tvm.nd.array(c_np)
-  shape = batch_size,M,K,N
-  s, arg_bufs = batchMatmul_manual(batch_size, M, K, N)
-  func = tvm.build(s, arg_bufs)
-  func(a, b, c)
+    target = tvm.target.Target(target="llvm", host="llvm")
+    batch_size = 64
+    M = 64
+    K = 128
+    N = 256
+    a_np = np.random.rand(batch_size, M, K).astype(np.float32)
+    b_np = np.random.rand(batch_size, K, N).astype(np.float32)
+    c_np = np.zeros((batch_size, M, N), dtype=np.float32)
+    a = tvm.nd.array(a_np)
+    b = tvm.nd.array(b_np)
+    c = tvm.nd.array(c_np)
+    shape = batch_size, M, K, N
+    s, arg_bufs = batchMatmul_manual(batch_size, M, K, N)
+    func = tvm.build(s, arg_bufs)
+    func(a, b, c)
 
-if __name__ == '__main__':
-  main()
+
+if __name__ == "__main__":
+    main()
--- benchmarks/OpOptimization/BatchMatmul/TVM/CPU/main.py
+++ benchmarks/OpOptimization/BatchMatmul/TVM/CPU/main.py
@@ -11,102 +11,117 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the entry for AutoScheduler BatchMatMul on CPU.
-# Autoscheduler is TVM's next-generation performance tuning tool, 
+# Autoscheduler is TVM's next-generation performance tuning tool,
 # which can automatically generate search spaces for optimizing tensor expressions.
 # TVM is an Apache-2.0 licensed project.
 # See the TVM license at: https://github.com/apache/tvm/blob/main/LICENSE
 #
 # ===---------------------------------------------------------------------------
-from batch_matmul_cpu  import *
+from batch_matmul_cpu import *
 import numpy
 import timeit
 import tvm.testing
 import mxnet as mx
 import time
+
+
 # ------------------------------------------------------------------------------
 # Helper Function
 # ------------------------------------------------------------------------------
 def evaluate_operation(s, vars, target, inputs, optimization, log):
-  """Evaluate operation correctness and print the performance information.
-  Args:
-    s: The schedule to be built.
-    vars: The argument lists to the function.
-    target: The target and option of the compilation.
-    inputs: The input tensors.
-    standard: The standard result for correctness evaluation.
-    optimization: The name of the optimization.
-    log: The log list.
-  """
-  func = tvm.build(s, vars, target=target)
-  data_x, data_k, data_y= inputs
-  dev = tvm.device(target.kind.name, 0)
-  evaluator = func.time_evaluator(func.entry_name, dev,number=10)
-  mean_time = evaluator(data_x, data_k, data_y).mean * 1000  # Convert to milliseconds
-  log.append((optimization, mean_time))
+    """Evaluate operation correctness and print the performance information.
+    Args:
+      s: The schedule to be built.
+      vars: The argument lists to the function.
+      target: The target and option of the compilation.
+      inputs: The input tensors.
+      standard: The standard result for correctness evaluation.
+      optimization: The name of the optimization.
+      log: The log list.
+    """
+    func = tvm.build(s, vars, target=target)
+    data_x, data_k, data_y = inputs
+    dev = tvm.device(target.kind.name, 0)
+    evaluator = func.time_evaluator(func.entry_name, dev, number=10)
+    mean_time = evaluator(data_x, data_k, data_y).mean * 1000  # Convert to milliseconds
+    log.append((optimization, mean_time))
+
 
 def report_performance(log):
-  """Convert the log into a performance table.
-  Args:
-    log: The log list.
-  """
-  baseline = log[-1][1]
-  header = "Benchmark".ljust(30) + "\t" + "Time".rjust(
-      10) + "\t" + "SpeedUp".rjust(10)
-  split_line = "-" * 70
-  print(split_line)
-  print(header)
-  print(split_line)
-  for result in log:
-    formatted_time = "{:.4f}".format(result[1])
-    formatted_performance = "{:.4f}".format(baseline / result[1])
-    print("\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m" %
-          (result[0].ljust(30), str(formatted_time + " ms").rjust(10),
-           str(formatted_performance).rjust(10)))
+    """Convert the log into a performance table.
+    Args:
+      log: The log list.
+    """
+    baseline = log[-1][1]
+    header = (
+        "Benchmark".ljust(30) + "\t" + "Time".rjust(10) + "\t" + "SpeedUp".rjust(10)
+    )
+    split_line = "-" * 70
+    print(split_line)
+    print(header)
+    print(split_line)
+    for result in log:
+        formatted_time = "{:.4f}".format(result[1])
+        formatted_performance = "{:.4f}".format(baseline / result[1])
+        print(
+            "\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m"
+            % (
+                result[0].ljust(30),
+                str(formatted_time + " ms").rjust(10),
+                str(formatted_performance).rjust(10),
+            )
+        )
+
 
 def main():
-  # ----------------------------------------------------------------------------
-  # Initialization and Baseline
-  # ----------------------------------------------------------------------------
-  # Initialize the log list.
-  log = []
-  target = tvm.target.Target(target="llvm", host="llvm")
-  batch_size = 64
-  M = 64
-  K = 128
-  N = 256
-  a_np = np.random.rand(batch_size, M, K).astype(np.float32)
-  b_np = np.random.rand(batch_size, K, N).astype(np.float32)
-  c_np = np.zeros((batch_size, M, N), dtype=np.float32)
-  a = tvm.nd.array(a_np)
-  b = tvm.nd.array(b_np)
-  c = tvm.nd.array(c_np)
-  shape = batch_size,M,K,N
-  
-  start_time = time.time()
-  shape = batch_size,M,K,N
-  batchMatmul_numpy(shape,a_np,b_np)
-  end_time = time.time()
-  numpy_time = end_time - start_time
-  sch, arg_bufs = batchMatmul_manual(batch_size, M, K, N)
-  evaluate_operation(sch,
-                      arg_bufs,
-                      target=target,
-                      inputs=(a,b,c),
-                      optimization="batchMatmul_manual",
-                      log=log)
+    # ----------------------------------------------------------------------------
+    # Initialization and Baseline
+    # ----------------------------------------------------------------------------
+    # Initialize the log list.
+    log = []
+    target = tvm.target.Target(target="llvm", host="llvm")
+    batch_size = 64
+    M = 64
+    K = 128
+    N = 256
+    a_np = np.random.rand(batch_size, M, K).astype(np.float32)
+    b_np = np.random.rand(batch_size, K, N).astype(np.float32)
+    c_np = np.zeros((batch_size, M, N), dtype=np.float32)
+    a = tvm.nd.array(a_np)
+    b = tvm.nd.array(b_np)
+    c = tvm.nd.array(c_np)
+    shape = batch_size, M, K, N
 
-  sch, arg_bufs = batchMatmul_auto_tuning(shape,"llvm")
-  evaluate_operation(sch,
-                      arg_bufs,
-                      target=target,
-                      inputs=(a,b,c),
-                      optimization="batchMatmul_auto_tuning",
-                      log=log)
+    start_time = time.time()
+    shape = batch_size, M, K, N
+    batchMatmul_numpy(shape, a_np, b_np)
+    end_time = time.time()
+    numpy_time = end_time - start_time
+    sch, arg_bufs = batchMatmul_manual(batch_size, M, K, N)
+    evaluate_operation(
+        sch,
+        arg_bufs,
+        target=target,
+        inputs=(a, b, c),
+        optimization="batchMatmul_manual",
+        log=log,
+    )
 
-  # Register numpy case.
-  log.append(("numpy_time", numpy_time  * 1000))  # Milliseconds
-  # Dump the performance table.
-  report_performance(log)
+    sch, arg_bufs = batchMatmul_auto_tuning(shape, "llvm")
+    evaluate_operation(
+        sch,
+        arg_bufs,
+        target=target,
+        inputs=(a, b, c),
+        optimization="batchMatmul_auto_tuning",
+        log=log,
+    )
+
+    # Register numpy case.
+    log.append(("numpy_time", numpy_time * 1000))  # Milliseconds
+    # Dump the performance table.
+    report_performance(log)
+
 
 if __name__ == "__main__":
-  main()
+    main()
