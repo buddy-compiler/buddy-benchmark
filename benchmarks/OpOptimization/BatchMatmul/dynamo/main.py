96eeba24d0f780afb70ca04d9ef0dba308fc84d4
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-25 15:37:21 +0800

    Update main.py

--- benchmarks/OpOptimization/BatchMatmul/dynamo/main.py
+++ benchmarks/OpOptimization/BatchMatmul/dynamo/main.py
@@ -1,24 +1,40 @@
+# You may obtain a copy of the License at
+#
+#     https://github.com/pytorch/pytorch/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the dynamo entry for benchmark Matmul on GPU.
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# users are surfaced through the torch.compiler namespace.
+# which can automatically generate search spaces for optimizing tensor expressions.
+# See the pytorch license at: https://github.com/pytorch/pytorch/blob/main/LICENSE
+#
+# ===---------------------------------------------------------------------------
 import numpy
 import time
 from torchDynamo import *
 # ------------------------------------------------------------------------------
 # User Configurable Variables
 # ------------------------------------------------------------------------------
-
 # Define the size of the matrix.
 # (M, K) x (K, N)
 B = 4
 M = 4
 N = 4
 K = 4
-
 dtype = "float32"
-
 
 # ------------------------------------------------------------------------------
 # Helper Function
 # ------------------------------------------------------------------------------
-
 def evaluator(s, inputs, num):
   a, b = inputs
   all_time = []
@@ -31,11 +47,8 @@ def evaluator(s, inputs, num):
       end = time.time()
       elapsed_time = end - start
       all_time.append(elapsed_time)
-
   average_time = sum(all_time) / num
-
   return average_time
-
 
 def evaluate_operation(s, inputs, optimization, log):
   """Evaluate operation correctness and print the performance information.
@@ -47,7 +60,6 @@ def evaluate_operation(s, inputs, optimization, log):
   """
   mean_time = evaluator(s, inputs, 10)
   log.append((optimization, mean_time))
-
 
 def report_performance(log):
   """Convert the log into a performance table.
@@ -68,43 +80,34 @@ def report_performance(log):
           (result[0].ljust(20), str(formatted_time + " ms").rjust(10),
            str(formatted_performance).rjust(10)))
 
-
 def main():
   # ----------------------------------------------------------------------------
   # Initialization and Baseline
   # ----------------------------------------------------------------------------
   # Initialize the log list.
   log = []
-
   # Generate random tensor for testing.
   a_np = numpy.random.rand(B, M, K)
   b_np = numpy.random.rand(B, K, N)
   a_tensor = torch.tensor(a_np)
   b_tensor = torch.tensor(b_np)
-
-
   # ----------------------------------------------------------------------------
   # Register Benchmarks and Dump Report
   # ----------------------------------------------------------------------------
   # Register default schedule.
-
   s = default_matrix_multiply()
   evaluate_operation(s,
                      inputs=(a_tensor, b_tensor),
                      optimization="torch_batchmm_default",
                      log=log)
 
-
   s = dynamo_batch_matrix_multiply()
   evaluate_operation(s,
                      inputs=(a_tensor, b_tensor),
                      optimization="torch_batchmm_dynamo",
                      log=log)
-
- 
-
+  
   report_performance(log)
-
 
 if __name__ == "__main__":
   main()
