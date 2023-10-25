f2cd058347081823d5a85c91f1a2c97442c31973
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-25 15:08:00 +0800

    Update torchDynamo.py

--- benchmarks/OpOptimization/BroadcastAdd/Dynamo/torchDynamo.py
+++ benchmarks/OpOptimization/BroadcastAdd/Dynamo/torchDynamo.py
@@ -1,7 +1,26 @@
+# You may obtain a copy of the License at
+#
+#     https://github.com/pytorch/pytorch/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the dynamo optimization for benchmark broadcastAdd on GPU.
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# users are surfaced through the torch.compiler namespace.
+# which can automatically generate search spaces for optimizing tensor expressions.
+# See the pytorch license at: https://github.com/pytorch/pytorch/blob/main/LICENSE
+#
+# ===---------------------------------------------------------------------------
 import torch
 import torch.nn as nn
 import numpy as np
-
 
 def get_bcast_data(shape1, shape2):
     """Return random tensors a, b
@@ -30,8 +49,6 @@ def broadcastAdd_compiled():
     f_compiled = torch.compile(f)
     return f_compiled
 
-
-
 def main():
     m = 3
     n = 4
@@ -43,15 +60,5 @@ def main():
     print(c)
     print(c.shape)
 
-
-
 if __name__ == "__main__":
   main()
-
-
-
-
-
-
-
-
