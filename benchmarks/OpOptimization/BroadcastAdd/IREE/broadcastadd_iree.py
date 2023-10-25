fdddf00821341004aac2aa72634c26e0cccad1c0
zwshan <zw.shan@outlook.com>
2023-10-25 21:01:11 +0800

    using formatter to format code

--- benchmarks/OpOptimization/BroadcastAdd/IREE/broadcastadd_iree.py
+++ benchmarks/OpOptimization/BroadcastAdd/IREE/broadcastadd_iree.py
@@ -11,9 +11,9 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the IREE optimization for benchmark broadcastAdd on GPU.
-# IREE (Intermediate Representation Execution Environment, pronounced as "eerie") 
-# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML) 
-# models to a unified IR that scales up to meet the needs of the datacenter and down 
+# IREE (Intermediate Representation Execution Environment, pronounced as "eerie")
+# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML)
+# models to a unified IR that scales up to meet the needs of the datacenter and down
 # to satisfy the constraints and special considerations of mobile and edge deployments.
 #
 # ===---------------------------------------------------------------------------
@@ -24,21 +24,27 @@ import iree_torch
 import io
 import numpy as np
 
+
 class BroadcastAdd(nn.Module):
     def __init__(self, weight):
         super(BroadcastAdd, self).__init__()
-        self.weight = nn.Parameter(weight)  
+        self.weight = nn.Parameter(weight)
+
     def forward(self, x):
         result = torch.add(x, self.weight)
         return result
 
+
 def torch_BroadcastAdd(b_dim1, b_dim2):
     weight = torch.randn(b_dim1, b_dim2)
     model = BroadcastAdd(weight)
-    return model,weight
+    return model, weight
+
 
 def iree_BroadcastAdd(model, example_input):
-    linalg_on_tensors_mlir = torch_mlir.compile(model,example_input,output_type="linalg-on-tensors",use_tracing=False)
+    linalg_on_tensors_mlir = torch_mlir.compile(
+        model, example_input, output_type="linalg-on-tensors", use_tracing=False
+    )
     iree_backend = "llvm-cpu"
     iree_vmfb = iree_torch.compile_to_vmfb(linalg_on_tensors_mlir, iree_backend)
     invoker = iree_torch.load_vmfb(iree_vmfb, iree_backend)
