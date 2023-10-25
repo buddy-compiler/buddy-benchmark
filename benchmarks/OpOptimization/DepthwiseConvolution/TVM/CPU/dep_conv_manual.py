855ff7f6169bcd30052784a8a5fff6b7b8d88d53
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-25 14:31:52 +0800

    Update dep_conv_manual.py

--- benchmarks/OpOptimization/DepthwiseConvolution/TVM/CPU/dep_conv_manual.py
+++ benchmarks/OpOptimization/DepthwiseConvolution/TVM/CPU/dep_conv_manual.py
@@ -1,11 +1,28 @@
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the manual optimization for benchmark DepthwiseConvolution on CPU.
+# Autoscheduler is TVM's next-generation performance tuning tool, 
+# which can automatically generate search spaces for optimizing tensor expressions.
+# TVM is an Apache-2.0 licensed project.
+# See the TVM license at: https://github.com/apache/tvm/blob/main/LICENSE
+#
+# ===---------------------------------------------------------------------------
 import numpy as np
 import tvm
 from tvm import te
 
-
 def depthwise_conv_default(ic, nh, nw, kh, kw, ph=0, pw=0, sh=1, sw=1):
     """Convolution
-
     ic : number of channels for both input and output
     nh, nw : input width and height
     kh, kw : kernel width and height
@@ -28,13 +45,10 @@ def depthwise_conv_default(ic, nh, nw, kh, kw, ph=0, pw=0, sh=1, sw=1):
             (PaddedX[c, i*sh+rkh, j*sw+rkw] * K[c, 0, rkh, rkw]),
             axis=[rkh, rkw]), name='Y')
     sch = te.create_schedule(Y.op)
-
     return sch,(X, K, Y)
-
 
 def padding(X, ph, pw, val=0):
     """Pad X with the given value in 2-D
-
     ph, pw : height and width padding
     val : padding value, default 0
     """
@@ -47,8 +61,6 @@ def padding(X, ph, pw, val=0):
                 val, X[i[:-2]+(i[-2]-ph, i[-1]-pw)]),
             name='PaddedX')
 
-
-
 def conv_out_size(n, k, p, s):
     """Compute the output size by given input size n (width or height),
     kernel size k, padding p, and stride s
@@ -56,12 +68,9 @@ def conv_out_size(n, k, p, s):
     """
     return (n - k + 2 * p)//s + 1
 
-
-
 def get_conv_data(oc, ic, n, k, p=0, s=1, constructor=None, conv_type='direct'):
     """Return random 3-D data tensor, 3-D kernel tenor and empty 3-D output
     tensor with the shapes specified by input arguments.
-
     oc, ic : output and input channels
     n : input width and height
     k : kernel width and height
@@ -82,12 +91,10 @@ def get_conv_data(oc, ic, n, k, p=0, s=1, constructor=None, conv_type='direct'):
         data, weight, out = (constructor(x) for x in [data, weight, out])
     return data, weight, out
 
-
 def depthwise_conv_pack(c, nh, nw, kh, kw, ph, pw, tc):
     """Pack data and weight for depthwise convolution
        Note that the input channel of kernel is specified as 1,
        and the output channel of kernel equals the input channel of data
-
     c : input channel of data and output channel of kernel
     nh, nw : input width and height
     kh, kw : kernel width and height
@@ -113,10 +120,8 @@ def depthwise_conv_pack(c, nh, nw, kh, kw, ph, pw, tc):
         name='PackedK')
     return X, K, PaddedX, PackedX, PackedK
 
-
 def depthwise_conv(c, nh, nw, kh, kw, ph, pw, sh, sw, tc):
     """depthwise conv
-
     c : number of channels for both input and output.
     nh, nw : input width and height
     kh, kw : kernel width and height
@@ -146,29 +151,24 @@ def depthwise_conv(c, nh, nw, kh, kw, ph, pw, sh, sw, tc):
                     name='Y')
     return X, K, Y, PaddedX, PackedX, PackedK, PackedY
 
-
 def depthwise_cached_block(c, n, k, p, s):
     # tiling sizes for channel and width
     tc, tw = 16, 4
     X, K, Y, PaddedX, PackedX, PackedK, PackedY = depthwise_conv(
         c, n, n, k, k, p, p, s, s, tc)
     sch = te.create_schedule(Y.op)
-
     CachedY = sch.cache_write(PackedY, 'global')
-
     c_out, h, w, c_in = sch[PackedY].op.axis
     w_out, w_in = sch[PackedY].split(w, factor=tw)
     sch[PackedY].reorder(c_out, h, w_out, w_in, c_in)
     c_out_h = sch[PackedY].fuse(c_out, h)
     sch[PackedY].parallel(c_out_h)
     sch[CachedY].compute_at(sch[PackedY], w_out)
-
     cc_out, ch, cw, cc_in = sch[CachedY].op.axis
     kh, kw = sch[CachedY].op.reduce_axis
     sch[CachedY].reorder(cc_out, ch, kh, kw, cw, cc_in)
     sch[CachedY].vectorize(cc_in)
     sch[CachedY].unroll(cw)
-
     # Schedule the padding by adding thread-level parallelism
     if PaddedX != X:
         sch[PaddedX].parallel(PaddedX.op.axis[0])
@@ -181,6 +181,3 @@ def depthwise_cached_block(c, n, k, p, s):
     sch[Y].parallel(sch[Y].fuse(*Y.op.axis[0:2]))
     sch[Y].unroll(Y.op.axis[-1])
     return sch, (X, K, Y)
-
-
-
