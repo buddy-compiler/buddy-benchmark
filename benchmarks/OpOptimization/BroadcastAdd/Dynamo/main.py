b65e39891c5dd6320201ecb9e8eb99b47f224a7a
zwshan <zw.shan@outlook.com>
2023-10-25 20:29:30 +0800

    using formatter to format my code

--- benchmarks/OpOptimization/BroadcastAdd/Dynamo/main.py
+++ benchmarks/OpOptimization/BroadcastAdd/Dynamo/main.py
@@ -11,8 +11,8 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the entry for benchmark broadcastAdd on GPU.
-# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
-# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch
 # users are surfaced through the torch.compiler namespace.
 # which can automatically generate search spaces for optimizing tensor expressions.
 # See the pytorch license at: https://github.com/pytorch/pytorch/blob/main/LICENSE
@@ -21,6 +21,7 @@
 import numpy
 import time
 from torchDynamo import *
+
 # ------------------------------------------------------------------------------
 # User Configurable Variables
 # ------------------------------------------------------------------------------
@@ -30,80 +31,87 @@ dtype = "float32"
 # Helper Function
 # ------------------------------------------------------------------------------
 
-def evaluator(s, inputs, num):
-  all_time = []
-  a,b = inputs
-  for i in range(num):
-      torch.cuda.synchronize()
-      start = time.time()
-      result = s(a,b)
-      torch.cuda.synchronize()
-      end = time.time()
-      elapsed_time = end - start
-      all_time.append(elapsed_time)
 
-  average_time = sum(all_time) / num
-  return average_time
+def evaluator(s, inputs, num):
+    all_time = []
+    a, b = inputs
+    for i in range(num):
+        torch.cuda.synchronize()
+        start = time.time()
+        result = s(a, b)
+        torch.cuda.synchronize()
+        end = time.time()
+        elapsed_time = end - start
+        all_time.append(elapsed_time)
+
+    average_time = sum(all_time) / num
+    return average_time
+
 
 def evaluate_operation(s, inputs, optimization, log):
-  """Evaluate operation correctness and print the performance information.
-  Args:
-    s: The schedule to be built.
-    inputs: The input tensors.
-    optimization: The name of the optimization.
-    log: The log list.
-  """
-  mean_time = evaluator(s, inputs, 1)
-  log.append((optimization, mean_time))
+    """Evaluate operation correctness and print the performance information.
+    Args:
+      s: The schedule to be built.
+      inputs: The input tensors.
+      optimization: The name of the optimization.
+      log: The log list.
+    """
+    mean_time = evaluator(s, inputs, 1)
+    log.append((optimization, mean_time))
+
 
 def report_performance(log):
-  """Convert the log into a performance table.
-  Args:
-    log: The log list.
-  """
-  baseline = log[-1][1]
-  header = "Benchmark".ljust(20) + "\t" + "Time".rjust(
-      10) + "\t" + "SpeedUp".rjust(10)
-  split_line = "-" * 50
-  print(split_line)
-  print(header)
-  print(split_line)
-  for result in log:
-    formatted_time = "{:.2f}".format(result[1])
-    formatted_performance = "{:.2f}".format(baseline / result[1])
-    print("\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m" %
-          (result[0].ljust(20), str(formatted_time + " ms").rjust(10),
-           str(formatted_performance).rjust(10)))
+    """Convert the log into a performance table.
+    Args:
+      log: The log list.
+    """
+    baseline = log[-1][1]
+    header = (
+        "Benchmark".ljust(20) + "\t" + "Time".rjust(10) + "\t" + "SpeedUp".rjust(10)
+    )
+    split_line = "-" * 50
+    print(split_line)
+    print(header)
+    print(split_line)
+    for result in log:
+        formatted_time = "{:.2f}".format(result[1])
+        formatted_performance = "{:.2f}".format(baseline / result[1])
+        print(
+            "\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m"
+            % (
+                result[0].ljust(20),
+                str(formatted_time + " ms").rjust(10),
+                str(formatted_performance).rjust(10),
+            )
+        )
+
 
 def main():
-  # ----------------------------------------------------------------------------
-  # Initialization and Baseline
-  # ----------------------------------------------------------------------------
-  # Initialize the log list.
-  log = []
-  # Generate random tensor for testing.
-  m = 256
-  n = 512
-  shape1 = (m, 1)
-  shape2 = (1, n)
-  a, b, c = get_bcast_data(shape1, shape2)
-  # ----------------------------------------------------------------------------
-  # Register Benchmarks and Dump Report
-  # ----------------------------------------------------------------------------
-  # Register default schedule.
-  s_1 = broadcastAdd_torch()
-  evaluate_operation(s_1,
-                     inputs=(a,b),
-                     optimization="torch_broAdd_default",
-                     log=log)
-  
-  s_2 = broadcastAdd_compiled()
-  evaluate_operation(s_2,
-                     inputs=(a,b),
-                     optimization="torch_broAdd_compiled",
-                     log=log)
-  
-  report_performance(log)
+    # ----------------------------------------------------------------------------
+    # Initialization and Baseline
+    # ----------------------------------------------------------------------------
+    # Initialize the log list.
+    log = []
+    # Generate random tensor for testing.
+    m = 256
+    n = 512
+    shape1 = (m, 1)
+    shape2 = (1, n)
+    a, b, c = get_bcast_data(shape1, shape2)
+    # ----------------------------------------------------------------------------
+    # Register Benchmarks and Dump Report
+    # ----------------------------------------------------------------------------
+    # Register default schedule.
+    s_1 = broadcastAdd_torch()
+    evaluate_operation(s_1, inputs=(a, b), optimization="torch_broAdd_default", log=log)
+
+    s_2 = broadcastAdd_compiled()
+    evaluate_operation(
+        s_2, inputs=(a, b), optimization="torch_broAdd_compiled", log=log
+    )
+
+    report_performance(log)
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- benchmarks/OpOptimization/BroadcastAdd/Dynamo/torchDynamo.py
+++ benchmarks/OpOptimization/BroadcastAdd/Dynamo/torchDynamo.py
@@ -11,8 +11,8 @@
 # ===---------------------------------------------------------------------------
 #
 # This file implements the dynamo optimization for benchmark broadcastAdd on GPU.
-# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation 
-# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch 
+# torchdynamo is an internal API that uses a CPython feature called the Frame Evaluation
+# API to safely capture PyTorch graphs. Methods that are available externally for PyTorch
 # users are surfaced through the torch.compiler namespace.
 # which can automatically generate search spaces for optimizing tensor expressions.
 # See the pytorch license at: https://github.com/pytorch/pytorch/blob/main/LICENSE
@@ -22,6 +22,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 def get_bcast_data(shape1, shape2):
     """Return random tensors a, b
     and empty tensor c to store broadcast results between a and b
@@ -30,24 +31,30 @@ def get_bcast_data(shape1, shape2):
     np.random.seed(0)
     a = np.random.normal(size=shape1).astype("float32")
     b = np.random.normal(size=shape2).astype("float32")
-    out_shape = (shape1[0] if shape2[0] == 1 else shape2[0],
-                 shape1[1] if shape2[1] == 1 else shape2[1])
-    c = np.empty(out_shape, dtype='float32')
+    out_shape = (
+        shape1[0] if shape2[0] == 1 else shape2[0],
+        shape1[1] if shape2[1] == 1 else shape2[1],
+    )
+    c = np.empty(out_shape, dtype="float32")
     a = torch.from_numpy(a)
     b = torch.from_numpy(b)
     c = torch.from_numpy(c)
     return a, b, c
 
+
 def broadcastAdd_torch():
-    def inner_add(a,b):
-        c = torch.add(a,b)
+    def inner_add(a, b):
+        c = torch.add(a, b)
         return c
+
     return inner_add
+
 
 def broadcastAdd_compiled():
     f = broadcastAdd_torch()
     f_compiled = torch.compile(f)
     return f_compiled
+
 
 def main():
     m = 3
@@ -60,5 +67,6 @@ def main():
     print(c)
     print(c.shape)
 
+
 if __name__ == "__main__":
-  main()
+    main()
