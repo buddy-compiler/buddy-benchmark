eae2c7fbcf902fd46633c74d7ab3d0666af682ee
zwshan <zw.shan@outlook.com>
2023-10-25 20:56:22 +0800

    using formatter to format code

---
+++ benchmarks/OpOptimization/BatchNormalization/IREE/batchNormalization_iree.py
@@ -0,0 +1,82 @@
+# You may obtain a copy of the License at
+#
+#     https://github.com/openxla/iree/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the IREE Optimization for benchmark BatchNormalization on GPU.
+# IREE (Intermediate Representation Execution Environment, pronounced as "eerie")
+# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML)
+# models to a unified IR that scales up to meet the needs of the datacenter and down
+# to satisfy the constraints and special considerations of mobile and edge deployments.
+#
+# ===---------------------------------------------------------------------------
+import torch
+import torch.nn as nn
+import torch_mlir
+import iree_torch
+import io
+import numpy as np
+
+
+class CustomBatchNorm(nn.Module):
+    def __init__(self, mean, var, gamma, beta, eps=1e-5):
+        super(CustomBatchNorm, self).__init__()
+        self.mean = nn.Parameter(mean)
+        self.var = nn.Parameter(var)
+        self.gamma = nn.Parameter(gamma)
+        self.beta = nn.Parameter(beta)
+        self.eps = nn.Parameter(
+            torch.tensor(eps, dtype=torch.float32), requires_grad=True
+        )
+
+    def forward(self, x):
+        C1 = x - self.mean
+        C2 = torch.sqrt(self.var + self.eps)
+        y = C1 / C2 * self.gamma + self.beta
+        return y
+
+
+def torch_CustomBatchNorm():
+    model = CustomBatchNorm
+    return model, weight
+
+
+def iree_BatchNorm(model, example_input):
+    linalg_on_tensors_mlir = torch_mlir.compile(
+        model, example_input, output_type="linalg-on-tensors", use_tracing=False
+    )
+    iree_backend = "cuda"  # "llvm-cpu"
+    iree_vmfb = iree_torch.compile_to_vmfb(linalg_on_tensors_mlir, iree_backend)
+    invoker = iree_torch.load_vmfb(iree_vmfb, iree_backend)
+    return invoker
+
+
+def get_bn_data(c, n):
+    """
+    Return the batch norm data, mean, variance, gamma and beta tensors.
+    Also return the empty tensor for output.
+    c : channels
+    n : input width and height
+    """
+    np.random.seed(0)
+    data = np.random.normal(size=(c, n, n)).astype("float32")
+    mean = np.random.normal(size=(c, 1, 1)).astype("float32")
+    var = np.random.normal(loc=1.0, size=(c, 1, 1)).astype("float32")
+    var = np.absolute(var)
+    gamma = np.random.normal(size=(c, 1, 1)).astype("float32")
+    beta = np.random.normal(size=(c, 1, 1)).astype("float32")
+    out = np.empty((c, n, n), dtype="float32")
+    data = torch.from_numpy(data)
+    mean = torch.from_numpy(mean)
+    var = torch.from_numpy(var)
+    gamma = torch.from_numpy(gamma)
+    beta = torch.from_numpy(beta)
+    out = torch.from_numpy(out)
+    return data, mean, var, gamma, beta, out
---
+++ benchmarks/OpOptimization/BatchNormalization/IREE/main.py
@@ -0,0 +1,131 @@
+# You may obtain a copy of the License at
+#
+#     https://github.com/openxla/iree/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ===---------------------------------------------------------------------------
+#
+# This file implements the IREE entry for benchmark BatchNormalization on GPU.
+# IREE (Intermediate Representation Execution Environment, pronounced as "eerie")
+# is an MLIR-based end-to-end compiler and runtime that lowers Machine Learning (ML)
+# models to a unified IR that scales up to meet the needs of the datacenter and down
+# to satisfy the constraints and special considerations of mobile and edge deployments.
+#
+# ===---------------------------------------------------------------------------
+import numpy as np
+import time
+from batchNormalization_iree import *
+
+dtype = "float32"
+
+
+def iree_evaluator(s, inputs, num):
+    result = s.forward(inputs)
+    all_time = []
+    for i in range(num):
+        start = time.time()
+        s.forward(inputs)
+        end = time.time()
+        elapsed_time = end - start
+        all_time.append(elapsed_time)
+    average_time = sum(all_time) / num
+    return average_time
+
+
+def numpy_evaluator(a_tensor, b_tensor, num):
+    a_tensor_np = a_tensor.numpy()
+    b_tensor_np = b_tensor.numpy()
+    all_time = []
+    for i in range(num):
+        start = time.time()
+        np.dot(a_tensor_np, b_tensor_np)
+        end = time.time()
+        elapsed_time = end - start
+        all_time.append(elapsed_time)
+    average_time = sum(all_time) / num
+    return average_time
+
+
+def evaluator(s, inputs, num):
+    result = s(inputs)
+    all_time = []
+    for i in range(num):
+        start = time.time()
+        s(inputs)
+        end = time.time()
+        elapsed_time = end - start
+        all_time.append(elapsed_time)
+    average_time = sum(all_time) / num
+    return average_time
+
+
+def evaluate_operation(s, inputs, optimization, log):
+    """Evaluate operation correctness and print the performance information.
+    Args:
+      s: The schedule to be built.
+      inputs: The input tensors.
+      optimization: The name of the optimization.
+      log: The log list.
+    """
+    if optimization == "IREE":
+        mean_time = iree_evaluator(s, inputs, 10)
+    else:
+        mean_time = evaluator(s, inputs, 10)
+    log.append((optimization, mean_time))
+
+
+def report_performance(log):
+    """Convert the log into a performance table.
+    Args:
+      log: The log list.
+    """
+    baseline = log[-1][1]
+    header = (
+        "Benchmark".ljust(20) + "\t" + "Time".rjust(10) + "\t" + "SpeedUp".rjust(10)
+    )
+    split_line = "-" * 50
+    print(split_line)
+    print(header)
+    print(split_line)
+    for result in log:
+        formatted_time = "{:.2f}".format(result[1])
+        formatted_performance = "{:.2f}".format(baseline / result[1])
+        print(
+            "\033[32m%s\033[0m\t\033[33m%s\033[0m\t\033[34m%s\033[0m"
+            % (
+                result[0].ljust(20),
+                str(formatted_time + " ms").rjust(10),
+                str(formatted_performance).rjust(10),
+            )
+        )
+
+
+def main():
+    # ----------------------------------------------------------------------------
+    # Initialization and Baseline
+    # ----------------------------------------------------------------------------
+    # Initialize the log list.
+    log = []
+    # Generate random tensor for testing.
+    size = (256, 128)
+    data, mean, var, gamma, beta, out = get_bn_data(size[0], size[1])
+    example_input = data
+    model = CustomBatchNorm(mean, var, gamma, beta)
+    # ----------------------------------------------------------------------------
+    # Register Benchmarks and Dump Report
+    # ----------------------------------------------------------------------------
+    # Register default schedule.
+    invoker = iree_BatchNorm(model, example_input)
+    evaluate_operation(invoker, inputs=example_input, optimization="IREE", log=log)
+    s = model
+    evaluate_operation(s, inputs=example_input, optimization="torch_cpu", log=log)
+    report_performance(log)
+
+
+if __name__ == "__main__":
+    main()
