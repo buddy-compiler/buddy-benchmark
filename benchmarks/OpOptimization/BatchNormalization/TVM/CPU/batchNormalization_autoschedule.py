752b11ed4f780ebcdf97a9878dcb27510e7d78af
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-31 17:37:15 +0800

    Update batchNormalization_autoschedule.py

--- benchmarks/OpOptimization/BatchNormalization/TVM/CPU/batchNormalization_autoschedule.py
+++ benchmarks/OpOptimization/BatchNormalization/TVM/CPU/batchNormalization_autoschedule.py
@@ -1,4 +1,4 @@
-# ===- batchNormalization_autoschedule.py --------------------------------------------------------
+# ===- batchNormalization_autoschedule.py --------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
