f2536b3d57f2de3b22c60cbbd8b1271219b6c641
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-31 17:42:20 +0800

    Update gpu_matmul.py

--- benchmarks/OpOptimization/MatMul/TVM/GPU/gpu_matmul.py
+++ benchmarks/OpOptimization/MatMul/TVM/GPU/gpu_matmul.py
@@ -1,4 +1,4 @@
-# ===- gpu_matmul.py --------------------------------------------------------
+# ===- gpu_matmul.py -----------------------------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
