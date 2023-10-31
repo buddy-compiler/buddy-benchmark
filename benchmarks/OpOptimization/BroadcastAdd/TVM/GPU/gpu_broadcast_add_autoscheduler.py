0adee07e13736a3221629eb1e4a69724f4d58942
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-31 17:16:55 +0800

    Update gpu_broadcast_add_autoscheduler.py

--- benchmarks/OpOptimization/BroadcastAdd/TVM/GPU/gpu_broadcast_add_autoscheduler.py
+++ benchmarks/OpOptimization/BroadcastAdd/TVM/GPU/gpu_broadcast_add_autoscheduler.py
@@ -1,4 +1,4 @@
-# ===- gpu_broadcast_add_autoscheduler.py --------------------------------------------------------
+# ===- gpu_broadcast_add_autoscheduler.py --------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
