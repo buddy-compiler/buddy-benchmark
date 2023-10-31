5c8c30cfe7f43dfa005b87d44b005c88331e8cf4
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-31 17:15:26 +0800

    Update broadcastadd_autoschedule.py

--- benchmarks/OpOptimization/BroadcastAdd/TVM/CPU/broadcastadd_autoschedule.py
+++ benchmarks/OpOptimization/BroadcastAdd/TVM/CPU/broadcastadd_autoschedule.py
@@ -1,4 +1,4 @@
-# ===- broadcastadd_autoschedule.py --------------------------------------------------------
+# ===- broadcastadd_autoschedule.py --------------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
