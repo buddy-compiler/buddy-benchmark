72a442dc817e26674232a5d889a761cfa54856fe
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-31 17:18:42 +0800

    Update dep_conv_mxnet.py

--- benchmarks/OpOptimization/DepthwiseConvolution/TVM/CPU/dep_conv_mxnet.py
+++ benchmarks/OpOptimization/DepthwiseConvolution/TVM/CPU/dep_conv_mxnet.py
@@ -1,4 +1,4 @@
-# ===- dep_conv_manual.py --------------------------------------------------------
+# ===- dep_conv_manual.py ------------------------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
