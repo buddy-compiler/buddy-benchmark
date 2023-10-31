d6567db50dfc474b1ba5129b86defc678c36c7bf
zwshan <shanzhw@mail2.sysu.edu.cn>
2023-10-31 05:50:53 +0000

    modify license

--- benchmarks/OpOptimization/BatchMatmul/TVM/CPU/main.py
+++ benchmarks/OpOptimization/BatchMatmul/TVM/CPU/main.py
@@ -1,4 +1,4 @@
-# ===- batch_matmul_cpu.py --------------------------------------------------------
+# ===-main.py --------------------------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
