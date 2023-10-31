d0274864f5dbfd497dd7089a4ba2eca1604494c8
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-31 17:19:39 +0800

    Update gpu_dep_conv_auto.py

--- benchmarks/OpOptimization/DepthwiseConvolution/TVM/GPU/gpu_dep_conv_auto.py
+++ benchmarks/OpOptimization/DepthwiseConvolution/TVM/GPU/gpu_dep_conv_auto.py
@@ -1,4 +1,4 @@
-# ===- gpu_dep_conv_auto.py --------------------------------------------------------
+# ===- gpu_dep_conv_auto.py ----------------------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
