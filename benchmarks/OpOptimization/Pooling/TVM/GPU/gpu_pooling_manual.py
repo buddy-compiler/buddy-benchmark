c1de29e6cf0fbec1a471d60239c551d549696496
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-31 17:36:22 +0800

    Update gpu_pooling_manual.py

--- benchmarks/OpOptimization/Pooling/TVM/GPU/gpu_pooling_manual.py
+++ benchmarks/OpOptimization/Pooling/TVM/GPU/gpu_pooling_manual.py
@@ -1,4 +1,4 @@
-# ===- gpu_pooling_manual.py --------------------------------------------------------
+# ===- gpu_pooling_manual.py ---------------------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
