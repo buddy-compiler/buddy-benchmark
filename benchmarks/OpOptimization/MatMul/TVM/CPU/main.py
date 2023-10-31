d0556fcbafa106869623fa9ad47d5b939f8ec0f9
zwshan <shanzhw@mail2.sysu.edu.cn>
2023-10-31 05:46:06 +0000

    modify license

--- benchmarks/OpOptimization/MatMul/TVM/CPU/main.py
+++ benchmarks/OpOptimization/MatMul/TVM/CPU/main.py
@@ -14,7 +14,7 @@
 #
 # ===---------------------------------------------------------------------------
 #
-# This file implements the TVM optimization entry for MatMul on GPU.
+# This file implements the TVM optimization entry for MatMul on CPU.
 # This file is based on the TVM tutorial:
 # https://tvm.apache.org/docs/tutorial/tensor_expr_get_started.html
 # TVM is an Apache-2.0 licensed project.
