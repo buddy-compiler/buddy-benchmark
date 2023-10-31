7aefd48a3de2a6f8b49442134f90194e6480d925
zwshan <62176674+zwshan@users.noreply.github.com>
2023-10-31 17:33:23 +0800

    Update matmul_iree.py

--- benchmarks/OpOptimization/MatMul/IREE/matmul_iree.py
+++ benchmarks/OpOptimization/MatMul/IREE/matmul_iree.py
@@ -1,4 +1,4 @@
-# ===- matmul_iree.py --------------------------------------------------------
+# ===- matmul_iree.py ----------------------------------------------------------
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
