72a1a1ce6fec1e3793422c7d37f1dbd4778b8b51
zwshan <shanzhw@mail2.sysu.edu.cn>
2023-10-31 06:18:33 +0000

    modify license

--- benchmarks/OpOptimization/DepthwiseConvolution/TVM/GPU/main.py
+++ benchmarks/OpOptimization/DepthwiseConvolution/TVM/GPU/main.py
@@ -1,3 +1,7 @@
+# ===- main.py --------------------------------------------------------
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
@@ -10,13 +14,14 @@
 #
 # ===---------------------------------------------------------------------------
 #
-# This file implements the entry for benchmark DepthwiseConvolution on GPU.
-# Autoscheduler is TVM's next-generation performance tuning tool,
-# which can automatically generate search spaces for optimizing tensor expressions.
+# This file implements the TVM optimization entry for depthwise Conv on GPU.
+# This file is based on the TVM tutorial:
+# https://tvm.apache.org/docs/tutorial/tensor_expr_get_started.html
 # TVM is an Apache-2.0 licensed project.
 # See the TVM license at: https://github.com/apache/tvm/blob/main/LICENSE
 #
 # ===---------------------------------------------------------------------------
+
 from gpu_dep_conv_manual import *
 from gpu_dep_conv_auto import *
 from dep_conv_mxnet import *
