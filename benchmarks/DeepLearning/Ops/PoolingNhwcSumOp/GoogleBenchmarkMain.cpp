c4ac56881cb4ccb0d6a5461ff1e2cda426d6c962
FloatingcloudKnight <1348185166@qq.com>
2024-07-05 12:21:25 +0000

    [DeepLearning/Ops] Add PoolingNhwcSumOp benchmark

--- benchmarks/DeepLearning/Ops/PoolingNhwcSumOp/GoogleBenchmarkMain.cpp
+++ benchmarks/DeepLearning/Ops/PoolingNhwcSumOp/GoogleBenchmarkMain.cpp
@@ -14,7 +14,7 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file implements the benchmark for conv2d(nhwc-hwcf) operation.
+// This file implements the benchmark for sum pooling (nhwc) operation.
 //
 //===----------------------------------------------------------------------===//
 
@@ -30,12 +30,10 @@
 #define INPUT_C 16
 #define FILTER_H 4
 #define FILTER_W 4
-#define FILTER_C 16
-#define FILTER_F 64
 #define OUTPUT_N 1
+#define OUTPUT_C 16
 #define OUTPUT_H 29
 #define OUTPUT_W 29
-#define OUTPUT_F 64
 
 // Helper functions and variables.
 namespace {
@@ -55,22 +53,22 @@ bool areArraysEqual(float array1[], float array2[], int size) {
 namespace {
 // Declare the mobilenet C interface.
 extern "C" {
-void _mlir_ciface_conv_2d_nhwc_hwcf_scalar(MemRef<float, 4> *input,
-                                           MemRef<float, 4> *filter,
-                                           MemRef<float, 4> *output);
-void _mlir_ciface_conv_2d_nhwc_hwcf_auto_vectorization(
-    MemRef<float, 4> *input, MemRef<float, 4> *filter,
-    MemRef<float, 4> *output);
+void _mlir_ciface_pooling_nhwc_sum_scalar(MemRef<float, 4> *input,
+                                          MemRef<float, 2> *filter,
+                                          MemRef<float, 4> *output);
+void _mlir_ciface_pooling_nhwc_sum_auto_vectorization(MemRef<float, 4> *input,
+                                                      MemRef<float, 2> *filter,
+                                                      MemRef<float, 4> *output);
 }
 
-#define DEFINE_CONV_2D_NHWC_HWCF_BENCHMARK(name, func)                         \
-  void BM_CONV_2D_NHWC_HWCF_##name(benchmark::State &state) {                  \
+#define DEFINE_POOLING_NHWC_SUM_BENCHMARK(name, func)                          \
+  void BM_POOLING_NHWC_SUM_##name(benchmark::State &state) {                   \
     intptr_t sizesInput[4] = {INPUT_N, INPUT_H, INPUT_W, INPUT_C};             \
-    intptr_t sizesFilter[4] = {FILTER_H, FILTER_W, FILTER_C, FILTER_F};        \
-    intptr_t sizesOutput[4] = {OUTPUT_N, OUTPUT_H, OUTPUT_W, OUTPUT_F};        \
+    intptr_t sizesFilter[2] = {FILTER_H, FILTER_W};                            \
+    intptr_t sizesOutput[4] = {OUTPUT_N, OUTPUT_C, OUTPUT_H, OUTPUT_W};        \
                                                                                \
     MemRef<float, 4> input(sizesInput, 1.0);                                   \
-    MemRef<float, 4> filter(sizesFilter, 1.0);                                 \
+    MemRef<float, 2> filter(sizesFilter, 1.0);                                 \
     MemRef<float, 4> output(sizesOutput, 0.0);                                 \
                                                                                \
     for (auto _ : state) {                                                     \
@@ -78,16 +76,14 @@ void _mlir_ciface_conv_2d_nhwc_hwcf_auto_vectorization(
     }                                                                          \
   }
 
-DEFINE_CONV_2D_NHWC_HWCF_BENCHMARK(SCALAR,
-                                   _mlir_ciface_conv_2d_nhwc_hwcf_scalar)
-DEFINE_CONV_2D_NHWC_HWCF_BENCHMARK(
-    AutoVectorization, _mlir_ciface_conv_2d_nhwc_hwcf_auto_vectorization)
+DEFINE_POOLING_NHWC_SUM_BENCHMARK(SCALAR, _mlir_ciface_pooling_nhwc_sum_scalar)
+DEFINE_POOLING_NHWC_SUM_BENCHMARK(
+    AutoVectorization, _mlir_ciface_pooling_nhwc_sum_auto_vectorization)
 } // namespace
 
 // Register benchmark cases.
-BENCHMARK(BM_CONV_2D_NHWC_HWCF_SCALAR)->Unit(benchmark::kMillisecond);
-BENCHMARK(BM_CONV_2D_NHWC_HWCF_AutoVectorization)
-    ->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_POOLING_NHWC_SUM_SCALAR)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_POOLING_NHWC_SUM_AutoVectorization)->Unit(benchmark::kMillisecond);
 
 /// Correctness Verification
 /// The verification does not affect the performance.
@@ -104,8 +100,8 @@ void verification() {
 
   // Set the layout sizes of input and output memref container.
   intptr_t sizesInput[4] = {INPUT_N, INPUT_H, INPUT_W, INPUT_C};
-  intptr_t sizesFilter[4] = {FILTER_H, FILTER_W, FILTER_C, FILTER_F};
-  intptr_t sizesOutput[4] = {OUTPUT_N, OUTPUT_H, OUTPUT_W, OUTPUT_F};
+  intptr_t sizesFilter[2] = {FILTER_H, FILTER_W};
+  intptr_t sizesOutput[4] = {OUTPUT_N, OUTPUT_C, OUTPUT_H, OUTPUT_W};
 
   // Generate input A and input B memref container with random numbers.
   const int inputSize = INPUT_N * INPUT_H * INPUT_W * INPUT_C;
@@ -115,23 +111,23 @@ void verification() {
   }
   MemRef<float, 4> inputMemRef(inputRand, sizesInput);
 
-  const int filterSize = FILTER_H * FILTER_W * FILTER_C * FILTER_F;
+  const int filterSize = FILTER_H * FILTER_W;
   float filterRand[filterSize];
   for (int i = 0; i < filterSize; ++i) {
     filterRand[i] = distribution(generator);
   }
-  MemRef<float, 4> filterMemRef(filterRand, sizesFilter);
+  MemRef<float, 2> filterMemRef(filterRand, sizesFilter);
 
   // Generate output memref container with zero.
-  const int outputSize = OUTPUT_N * OUTPUT_H * OUTPUT_W * OUTPUT_F;
+  const int outputSize = OUTPUT_N * OUTPUT_C * OUTPUT_H * OUTPUT_W;
   MemRef<float, 4> outputScalar(sizesOutput, 0.0);
   MemRef<float, 4> outputAutoVectorization(sizesOutput, 0.0);
 
   // Perform all the matmul implementation.
-  _mlir_ciface_conv_2d_nhwc_hwcf_scalar(&inputMemRef, &filterMemRef,
-                                        &outputScalar);
-  _mlir_ciface_conv_2d_nhwc_hwcf_auto_vectorization(&inputMemRef, &filterMemRef,
-                                                    &outputAutoVectorization);
+  _mlir_ciface_pooling_nhwc_sum_scalar(&inputMemRef, &filterMemRef,
+                                       &outputScalar);
+  _mlir_ciface_pooling_nhwc_sum_auto_vectorization(&inputMemRef, &filterMemRef,
+                                                   &outputAutoVectorization);
 
   // Get the result array.
   auto resultScalar = outputScalar.getData();
