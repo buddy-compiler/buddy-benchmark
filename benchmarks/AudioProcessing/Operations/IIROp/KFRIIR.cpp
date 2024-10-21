36e5fe8ca6b8edcab0abca1ea90be3b5a47fc363
taiqzheng <2013898008@qq.com>
2024-10-21 13:50:29 +0000

    [DAP] Seperate 'dap-op-iir-benchmark'  from 'audio-processing-benchmark'.

---
+++ benchmarks/AudioProcessing/Operations/IIROp/KFRIIR.cpp
@@ -0,0 +1,73 @@
+//===- KFRIIR.cpp ---------------------------------------------------------===//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the benchmark for KFR Iir function.
+//
+//===----------------------------------------------------------------------===//
+
+#include <benchmark/benchmark.h>
+#include <kfr/base.hpp>
+#include <kfr/dft.hpp>
+#include <kfr/dsp.hpp>
+#include <kfr/io.hpp>
+#include <iostream>
+
+using namespace kfr;
+
+constexpr size_t maxorder = 32;
+univector<float, 2000000> aud_iir;
+univector<float, 2000000> output;
+
+zpk<float> filt = iir_lowpass(bessel<float>(24), 1000, 48000);
+std::vector<biquad_params<float>> bqs = to_sos(filt);
+
+// Initialize univector.
+void initializeKFRIIR() {
+  audio_reader_wav<float> reader(open_file_for_reading(
+      "../../benchmarks/AudioProcessing/Audios/NASA_Mars.wav"));
+  reader.read(aud_iir.data(), aud_iir.size());
+}
+
+// Benchmarking function.
+static void KFR_IIR(benchmark::State &state) {
+  for (auto _ : state) {
+    for (int i = 0; i < state.range(0); ++i) {
+      output = biquad<maxorder>(bqs, aud_iir);
+    }
+  }
+}
+
+// Register benchmarking function.
+BENCHMARK(KFR_IIR)->Arg(1)->Unit(benchmark::kMillisecond);
+
+// Generate result wav file.
+void generateResultKFRIIR() {
+  println("-------------------------------------------------------");
+  println("[ KFR IIR Result Information ]");
+  univector<float> generateResult = biquad<maxorder>(bqs, aud_iir);
+
+  audio_writer_wav<float> writer(open_file_for_writing("./ResultKFRIir.wav"),
+                                 audio_format{1 /* channel */,
+                                              audio_sample_type::i24,
+                                              100000 /* sample rate */});
+  writer.write(generateResult.data(), generateResult.size());
+  println("Sample Rate  = ", writer.format().samplerate);
+  println("Channels     = ", writer.format().channels);
+  println("Length       = ", writer.format().length);
+  println("Duration (s) = ",
+          writer.format().length / writer.format().samplerate);
+  println("Bit depth    = ", audio_sample_bit_depth(writer.format().type));
+}
