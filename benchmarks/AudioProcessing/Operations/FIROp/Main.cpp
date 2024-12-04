edea3f69bca3ea1eee8705fa46eb55c46c2a51c4
Taiqi Zheng <56971484+taiqzheng@users.noreply.github.com>
2024-12-04 20:33:42 +0800

    [DAP] Update FIR benchmark. (#157)
    * [DAP] Update FIR benchmark.
    
    Co-authored-by: ShiHaoGao <1574987177@qq.com>
    
    * [AudioProcessing] Update hand-written mlir file 'MLIRFIR.mlir'.
    
    * [DAP] Adapt KFR 4.0.0 version.
    
    * [DAP] Update 'README.md' for fir operation.
    
    * [DAP] Add 'setprecision()' function for detailed verification information.
    
    ---------
    
    Co-authored-by: ShiHaoGao <1574987177@qq.com>

--- benchmarks/AudioProcessing/Operations/FIROp/Main.cpp
+++ benchmarks/AudioProcessing/Operations/FIROp/Main.cpp
@@ -14,23 +14,103 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This is the main file of the audio processing benchmark.
+// This is the main file of FIROp benchmark.
 //
 //===----------------------------------------------------------------------===//
 
+#include "Utils.hpp"
 #include <benchmark/benchmark.h>
 
-void initializeBuddyFIR();
+using namespace std;
 
-void generateResultBuddyFIR();
+// -----------------------------------------------------------------------------
+// Benchmark Configuration. Modify the numbers here for a custom benchmark.
+// -----------------------------------------------------------------------------
+
+#define _NUM_ITER 10
+#define _IN_OUT_SIZE 2000000
+#define _KERNEL_SIZE 127
+#define _PRINT true
+
+// -----------------------------------------------------------------------------
+// Global Variables and Functions. Please do not modify the code here.
+// -----------------------------------------------------------------------------
+
+univector<float, _IN_OUT_SIZE> firInput;
+univector<float, _IN_OUT_SIZE> firOutput;
+univector<float, _KERNEL_SIZE> taps127;
+
+intptr_t sizeofAud[1] = {_IN_OUT_SIZE};
+intptr_t sizeofKernel[1] = {_KERNEL_SIZE};
+
+MemRef<float, 1> audRef(sizeofAud);
+MemRef<float, 1> resRef(sizeofAud);
+MemRef<float, 1> kernelRef(sizeofKernel);
+
+using MLIRFunctionType = void (*)(MemRef<float, 1> *, MemRef<float, 1> *,
+                                  MemRef<float, 1> *);
+
+// Benchmarking function for MLIR based FIR method.
+void DAP_OPS_FIR(benchmark::State &state, MLIRFunctionType func) {
+  MemRef<float, 1> resRef(sizeofAud, 0.0);
+  for (auto _ : state) {
+    func(&audRef, &kernelRef, &resRef);
+  }
+  benchmark::DoNotOptimize(resRef);
+}
+
+// Benchmarking function for KFR FIR method.
+static void KFR_FIR(benchmark::State &state) {
+  for (auto _ : state) {
+    firOutput = kfr::fir(firInput, taps127);
+  }
+}
+
+// Verifies the result of an MLIR-based function against expected output.
+void Verification(const univector<float, _IN_OUT_SIZE> &outputExpected,
+                  MLIRFunctionType MLIRFunc, const std::string &name) {
+  // Initialize MemRef with all zeros.
+  MemRef<float, 1> outputGenerated(sizeofAud, 0.0);
+  MLIRFunc(&audRef, &kernelRef, &outputGenerated);
+  firOp::printMemRef(outputGenerated, name, /*doPrint=*/_PRINT);
+  firOp::verify(outputExpected, outputGenerated, _IN_OUT_SIZE, name);
+}
+
+// -----------------------------------------------------------------------------
+// Register Benchmark.
+// -----------------------------------------------------------------------------
+
+BENCHMARK_CAPTURE(DAP_OPS_FIR, mlir_fir, _mlir_ciface_mlir_fir)
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(_NUM_ITER);
+BENCHMARK_CAPTURE(DAP_OPS_FIR, buddy_fir, dap::FIR<float, 1>)
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(_NUM_ITER);
+BENCHMARK(KFR_FIR)->Unit(benchmark::kMillisecond)->Iterations(_NUM_ITER);
+
+// -----------------------------------------------------------------------------
+// Main Function.
+// -----------------------------------------------------------------------------
 
 int main(int argc, char **argv) {
-  initializeBuddyFIR();
+  // Initialize univectors and MemRefs.
+  firOp::initializeKFRFIR(firInput, taps127);
+  audRef = std::move(MemRef<float, 1>(firInput.data(), sizeofAud));
+  kernelRef = std::move(MemRef<float, 1>(taps127.data(), sizeofKernel));
+  resRef = std::move(MemRef<float, 1>(sizeofAud, 0.0));
 
+  // Run benchmark.
   ::benchmark::Initialize(&argc, argv);
   ::benchmark::RunSpecifiedBenchmarks();
 
-  generateResultBuddyFIR();
+  std::cout << "\033[34m---------- Verification ----------\033[0m" << std::endl;
+  // Obtain KFR output results as expected results in verification.
+  firOutput = kfr::fir(firInput, taps127);
+  firOp::printUnivector(firOutput, /*doPrint=*/_PRINT);
+
+  // Verify the correctness of all methods.
+  Verification(firOutput, dap::FIR<float, 1>, "Buddy");
+  Verification(firOutput, _mlir_ciface_mlir_fir, "MLIR");
 
   return 0;
 }
